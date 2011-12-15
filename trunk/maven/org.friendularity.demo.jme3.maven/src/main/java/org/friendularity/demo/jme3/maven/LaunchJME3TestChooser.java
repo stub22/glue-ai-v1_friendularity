8fce8beb8a512c85f5cd4132fa1d9bc63db65013
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2011-12-15 19:40:48 +0000

    First demonstration of building+running JMonkey client code against revised Maven Central structure - snapshot.

---
+++ trunk/maven/org.friendularity.demo.jme3.maven/src/main/java/org/friendularity/demo/jme3/maven/LaunchJME3TestChooser.java
@@ -0,0 +1,15 @@
+package org.friendularity.demo.jme3.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class LaunchJME3TestChooser 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Launching JME3 TestChooser");
+		jme3test.TestChooser.main(args);
+		System.out.println( "JME3 TestChooser launch complete.");
+    }
+}
