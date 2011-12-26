c0805baff34544e8a0eaf9eb7bcc664738a39495
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2011-12-26 20:21:43 +0000

    HelloWorld webapp instantiated using Maven archetype:  lift-archetype-basic_2.8.1(2.3-M1). build, then run with "mvn jetty:run"

---
+++ maven/org.friendularity.demo.lifter/src/main/scala/org/friendularity/demo/lifter/snippet/HelloWorld.scala
@@ -0,0 +1,28 @@
+package org.friendularity.demo.lifter {
+package snippet {
+
+import _root_.scala.xml.{NodeSeq, Text}
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+import _root_.java.util.Date
+import org.friendularity.demo.lifter.lib._
+import Helpers._
+
+class HelloWorld {
+  lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+
+  // bind the date into the element with id "time"
+  def howdy = "#time *" #> date.map(_.toString)
+  
+  def stuffy = "#wacko *" #> "It is all stuffy up in here!";
+  
+
+  /*
+   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+
+   def howdy = "#time *" #> date.toString
+   */
+}
+
+}
+}
