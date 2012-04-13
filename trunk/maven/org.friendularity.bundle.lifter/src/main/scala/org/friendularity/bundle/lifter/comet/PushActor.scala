b9f0e5c31da2b4f75fc519df25be1c429ba295f6
RyanBiggs <RyanBiggs@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2012-04-13 01:39:52 +0000

    Draft [broken] bundle.lifter OSGi module in work

---
+++ trunk/maven/org.friendularity.bundle.lifter/src/main/scala/org/friendularity/bundle/lifter/comet/PushActor.scala
@@ -0,0 +1,43 @@
+package org.friendularity.bundle.lifter {
+package comet {
+
+
+import net.liftweb.common._
+import net.liftweb.http.js.JE._
+import net.liftweb.http._
+import S._
+import net.liftweb.http.js.JsCmds.SetHtml
+import net.liftweb.util._
+import Helpers._
+import _root_.java.util.Date
+import scala.xml._
+import scala.util.Random
+
+
+class PushActor extends CometActor {
+  override def defaultPrefix = Full("clk")   
+var r = new Random()
+var PushSeconds = (r.nextInt(5)+3)*1000
+    
+      
+  def render = bind("RandPush" -> RandPushSpan)
+
+      
+  def RandPushSpan = (<span id="RandPush">{PushSeconds}</span>)
+  
+ 
+
+  // redraw ping every 3-7 seconds
+  ActorPing.schedule(this, Tick, 1000L) 
+
+  override def lowPriority : PartialFunction[Any, Unit] = {
+    case Tick => {
+      partialUpdate(SetHtml("RandPush", Text((PushSeconds/1000).toString))) ;
+      ActorPing.schedule(this, Tick, PushSeconds) ;
+      PushSeconds = (r.nextInt(5)+3)*1000;
+    }
+  }
+}
+case object Tick
+}
+}
