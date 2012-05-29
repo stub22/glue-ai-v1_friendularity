1bbad82207fbb50fb12022581bf858a796931633
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2012-05-29 07:27:30 +0000

    Moved startPumaDemo() into a Framework "STARTED" callback, removed duplicate deps that are no longer needed to finagle the classpath order.

--- trunk/maven/org.friendularity.bundle.demo.ccrk/src/main/java/org/friendularity/bundle/demo/ccrk/CCRK_DemoActivator.java
+++ trunk/maven/org.friendularity.bundle.demo.ccrk/src/main/java/org/friendularity/bundle/demo/ccrk/CCRK_DemoActivator.java
@@ -6,11 +6,13 @@ import org.cogchar.bundle.app.puma.PumaAppContext;
 import org.cogchar.bundle.app.puma.PumaBooter;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.FrameworkListener;
 
 
 public class CCRK_DemoActivator extends BundleActivatorBase {
 
-	@Override public void start(BundleContext context) throws Exception {
+	@Override public void start(final BundleContext context) throws Exception {
 		// Will look for log4j.properties at root of this bundle.
 		// Any top-level OSGi app that wants to enable Log4J (and thereby make Jena happy, while
 		// retaining the power to configure Jena's logging level) should have the dependencies
@@ -18,7 +20,17 @@ public class CCRK_DemoActivator extends BundleActivatorBase {
 		forceLog4jConfig();
 		// Print some howdys
 		super.start(context);
-		startPumaDemo(context);
+		context.addFrameworkListener(new FrameworkListener() {
+			public void frameworkEvent(FrameworkEvent fe) {
+				int eventType = fe.getType();
+				logInfo("************************ Got frameworkEvent with eventType=" + eventType + ", bundle=" + fe.getBundle());
+				if (eventType == FrameworkEvent.STARTED) {
+					logInfo("********  OSGi Framework has STARTED, we are ready for the pumaDemo!");
+					startPumaDemo(context);
+				}
+			}
+		});		
+		
 		// Here we could start some extra app-specific (e.g.) Cogbot binding goodies, tell them to attach to PUMA 
 		// behavior system.  However, the Cogchar config system is intended to be sufficiently general to
 		// handle most initialization cases without help from bundle activators.
@@ -29,9 +41,8 @@ public class CCRK_DemoActivator extends BundleActivatorBase {
 /**
  * 	Start the PUMA application (Cogchar OpenGL rendering and behavior with Robokind animation and speech).
  * @param bundleCtx
- * @throws Exception 
  */	
-	protected void startPumaDemo(BundleContext bundleCtx) throws Exception {
+	protected void startPumaDemo(BundleContext bundleCtx) {
 		PumaBooter pumaBooter = new PumaBooter();
 		PumaBooter.BootResult bootResult = pumaBooter.bootUnderOSGi(bundleCtx);
 		logInfo("PUMA BootResult: " + bootResult);
