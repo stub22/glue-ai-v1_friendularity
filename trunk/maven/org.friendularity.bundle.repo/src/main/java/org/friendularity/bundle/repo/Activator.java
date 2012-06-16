56e2a09ff9dd7f4dd6bfd9ff30df48db9047ddd3
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2012-06-16 23:07:20 +0000

    repo/webapp project can now start PumaWebMapper without OpenGL.

--- trunk/maven/org.friendularity.bundle.repo/src/main/java/org/friendularity/bundle/repo/Activator.java
+++ trunk/maven/org.friendularity.bundle.repo/src/main/java/org/friendularity/bundle/repo/Activator.java
@@ -1,17 +1,23 @@
 package org.friendularity.bundle.repo;
 
-import org.friendularity.datmat.TestSheetRead;
+import org.cogchar.bundle.app.puma.PumaWebMapper;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
     public void start(BundleContext context) throws Exception {
-
+		initWebapp();
     }
 
     public void stop(BundleContext context) throws Exception {
         // TODO add deactivation code here
     }
+	
+	public void initWebapp () { 
+		ClassLoader resourceCL = org.cogchar.bundle.render.resources.ResourceBundleActivator.class.getClassLoader();
+		PumaWebMapper pwm = new PumaWebMapper();
+		pwm.connectWebStuff(resourceCL);
+	}
 
 }
