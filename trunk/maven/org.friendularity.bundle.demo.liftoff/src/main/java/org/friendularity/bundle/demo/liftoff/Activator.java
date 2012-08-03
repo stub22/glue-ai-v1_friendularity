d4c29a6052ec693ae23209330c9957d2820a6c6c
ryanbiggs <ryanbiggs@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2012-08-03 06:27:40 +0000

    Modules which require web app (o.f.bundle.demo.liftoff, o.f.bundle.repo) now perform actions necessary to launch Lifter as managed service. TODO: perform more Lifter init in Cog Char PumaBooter as required according to config options.

--- trunk/maven/org.friendularity.bundle.demo.liftoff/src/main/java/org/friendularity/bundle/demo/liftoff/Activator.java
+++ trunk/maven/org.friendularity.bundle.demo.liftoff/src/main/java/org/friendularity/bundle/demo/liftoff/Activator.java
@@ -1,22 +1,30 @@
 package org.friendularity.bundle.demo.liftoff;
 
+import org.cogchar.bind.lift.LifterLifecycle;
 import org.cogchar.bundle.app.puma.PumaWebMapper;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.robokind.api.common.osgi.lifecycle.OSGiComponent;
 
 public class Activator implements BundleActivator {
 
 	public void start(BundleContext context) throws Exception {
-		initWebapp();
+		initWebapp(context);
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		// TODO add deactivation code here
 	}
 
-	public void initWebapp() {
+	public void initWebapp(BundleContext context) {
 		ClassLoader hrkContentCL = com.hrkind.content.preview.PreviewContentBundleActivator.class.getClassLoader();
 		PumaWebMapper pwm = new PumaWebMapper();
+		pwm.connectLiftInterface(context); // We may want to do this in PUMA, but for now this may make sense
 		pwm.connectHrkindWebContent(hrkContentCL);
+		
+		// Tell the lifter lifecycle to start, once its dependencies are satisfied
+		LifterLifecycle lifecycle = new LifterLifecycle();
+    	OSGiComponent lifterComp = new OSGiComponent(context, lifecycle);
+    	lifterComp.start();
 	}
 }
--- trunk/maven/org.friendularity.bundle.repo/src/main/java/org/friendularity/bundle/repo/Activator.java
+++ trunk/maven/org.friendularity.bundle.repo/src/main/java/org/friendularity/bundle/repo/Activator.java
@@ -1,23 +1,34 @@
 package org.friendularity.bundle.repo;
 
+import org.cogchar.bind.lift.LifterLifecycle;
+import org.cogchar.bundle.app.puma.PumaBooter;
 import org.cogchar.bundle.app.puma.PumaWebMapper;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.robokind.api.common.osgi.lifecycle.OSGiComponent;
+
 
 public class Activator implements BundleActivator {
 
 	public void start(BundleContext context) throws Exception {
-		initWebapp();
+		initWebapp(context);
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		// TODO add deactivation code here
 	}
 
-	public void initWebapp() {
+	public void initWebapp(BundleContext context) {
+		// Since we are not running PumaBooter, we must at least start the query service to get sheet-based config going
+		PumaBooter.startQueryService(context);
 		ClassLoader hrkContentCL = com.hrkind.content.preview.PreviewContentBundleActivator.class.getClassLoader();
 		PumaWebMapper pwm = new PumaWebMapper();
-		pwm.connectLiftInterface();
+		pwm.connectLiftInterface(context);
 		pwm.connectHrkindWebContent(hrkContentCL);
+		
+		// Tell the lifter lifecycle to start, once its dependencies are satisfied
+		LifterLifecycle lifecycle = new LifterLifecycle();
+    	OSGiComponent lifterComp = new OSGiComponent(context, lifecycle);
+    	lifterComp.start();
 	}
 }
