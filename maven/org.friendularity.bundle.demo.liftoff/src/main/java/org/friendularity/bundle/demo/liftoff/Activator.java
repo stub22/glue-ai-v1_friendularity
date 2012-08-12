d0c2c997d838f52190b4eb37bc85504ac9cca42f
Ryan Biggs <ryan@robokindrobots.com>
2012-08-12 23:06:17 +0000

    Changes to o.f.bundle.repo Activator to hook in appropriate context to allow web app requests of configuration changes; o.f.bundle.demo.liftoff Activator formatting fix

--- maven/org.friendularity.bundle.demo.liftoff/src/main/java/org/friendularity/bundle/demo/liftoff/Activator.java
+++ maven/org.friendularity.bundle.demo.liftoff/src/main/java/org/friendularity/bundle/demo/liftoff/Activator.java
@@ -8,23 +8,23 @@ import org.robokind.api.common.osgi.lifecycle.OSGiComponent;
 
 public class Activator implements BundleActivator {
 
-	public void start(BundleContext context) throws Exception {
-		initWebapp(context);
-	}
+    public void start(BundleContext context) throws Exception {
+        initWebapp(context);
+    }
 
-	public void stop(BundleContext context) throws Exception {
-		// TODO add deactivation code here
-	}
+    public void stop(BundleContext context) throws Exception {
+        // TODO add deactivation code here
+    }
 
-	public void initWebapp(BundleContext context) {
-		ClassLoader hrkContentCL = com.hrkind.content.preview.PreviewContentBundleActivator.class.getClassLoader();
-		PumaWebMapper pwm = new PumaWebMapper();
-		pwm.connectLiftInterface(context); // We may want to do this in PUMA, but for now this may make sense
-		pwm.connectHrkindWebContent(hrkContentCL);
-		
-		// Tell the lifter lifecycle to start, once its dependencies are satisfied
-		LifterLifecycle lifecycle = new LifterLifecycle();
-    	OSGiComponent lifterComp = new OSGiComponent(context, lifecycle);
-    	lifterComp.start();
-	}
+    public void initWebapp(BundleContext context) {
+        ClassLoader hrkContentCL = com.hrkind.content.preview.PreviewContentBundleActivator.class.getClassLoader();
+        PumaWebMapper pwm = new PumaWebMapper();
+        pwm.connectLiftInterface(context); // We may want to do this in PUMA, but for now this may make sense
+        pwm.connectHrkindWebContent(hrkContentCL);
+
+        // Tell the lifter lifecycle to start, once its dependencies are satisfied
+        LifterLifecycle lifecycle = new LifterLifecycle();
+        OSGiComponent lifterComp = new OSGiComponent(context, lifecycle);
+        lifterComp.start();
+    }
 }
