8b10a4f4de593b379da79de4f76d8250c64dbeaf
ryanbiggs <ryanbiggs@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2012-08-14 01:22:42 +0000

    o.f.bundle.netconfig: Added CogCharNetworkConfigAmbassador to implement Cog Char Lifter network config interface. Activator now starts a managed instance of CogCharNetworkConfigAmbassador for use by Lifter / LifterLifecycle. o.f.b.netconfig dependency added to o.f.bundle.repo and o.f.bundle.demo.liftoff to activate o.f.b.netconfig when starting those projects.

--- trunk/maven/org.friendularity.bundle.netconfig/src/main/java/org/friendularity/bundle/netconfig/Activator.java
+++ trunk/maven/org.friendularity.bundle.netconfig/src/main/java/org/friendularity/bundle/netconfig/Activator.java
@@ -1,16 +1,27 @@
 package org.friendularity.bundle.netconfig;
 
+import org.cogchar.bind.lift.LiftAmbassador;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.robokind.api.common.lifecycle.ServiceLifecycleProvider;
+import org.robokind.api.common.lifecycle.utils.SimpleLifecycle;
+import org.robokind.api.common.osgi.lifecycle.OSGiComponent;
 
 public class Activator implements BundleActivator {
 
-    public void start(BundleContext context) throws Exception {
-        // TODO add activation code here
-    }
+	public void start(BundleContext context) throws Exception {
+		connectCogCharInterface(context);
+	}
 
-    public void stop(BundleContext context) throws Exception {
-        // TODO add deactivation code here
-    }
+	public void stop(BundleContext context) throws Exception {
+		// TODO add deactivation code here
+	}
 
+	private static void connectCogCharInterface(BundleContext context) {
+		// Connect a CogCharNetworkConfigAmbassador as a (no lifecycle) managed service, so lifter can use it
+		ServiceLifecycleProvider ambassadorCycle =
+				new SimpleLifecycle(new CogCharNetworkConfigAmbassador(), LiftAmbassador.LiftNetworkConfigInterface.class);
+		OSGiComponent cncaComp = new OSGiComponent(context, ambassadorCycle);
+		cncaComp.start();
+	}
 }
