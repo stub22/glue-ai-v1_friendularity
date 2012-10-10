e6100bcf67bdbfcf681e3b6c86c1d78d178937c7
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2012-10-10 06:23:12 +0000

    Updated to reflect Cogchar method changes.

--- maven/org.friendularity.bundle.repo/src/main/java/org/friendularity/bundle/repo/Activator.java
+++ maven/org.friendularity.bundle.repo/src/main/java/org/friendularity/bundle/repo/Activator.java
@@ -20,9 +20,7 @@ public class Activator implements BundleActivator {
 	public void initWebapp(BundleContext context) {
 		// Since we are not running PumaBooter, we must at least start the query service to get sheet-based config going
 		PumaAppContext pac = new PumaAppContext(context);
-		pac.startVanillaRepoClient();
-		// And now, also apply the global mode
-		pac.applyGlobalConfigAndStartService();
+		pac.startRepositoryConfigServices();
 		// ... and set our app context with PumaWebMapper, so lift can issue repo update requests
 		pac.getWebMapper().connectLiftInterface(context);
 		// Tell the lifter lifecycle to start, once its dependencies are satisfied
