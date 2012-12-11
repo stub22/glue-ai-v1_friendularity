56b59b52172ab42818815c6ce1451a923ab98537
ryanbiggs <ryanbiggs@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2012-12-11 08:24:33 +0000

    Added o.f.bundle.lifter, a Lifter-only web app launcher bundle for testing Lifter-in-Felix-shell functionality. Likely will only be present in the short run; o.f.bundle.repo could extend this if we wanted to make it permanent.

---
+++ trunk/maven/org.friendularity.bundle.lifter/src/main/java/org/friendularity/bundle/lifter/Activator.java
@@ -0,0 +1,55 @@
+package org.friendularity.bundle.lifter;
+
+import com.hp.hpl.jena.query.Dataset;
+import org.appdapter.core.name.FreeIdent;
+import org.appdapter.core.name.Ident;
+import org.appdapter.osgi.core.BundleActivatorBase;
+import org.cogchar.app.puma.boot.PumaAppContext;
+import org.cogchar.app.puma.cgchr.PumaWebMapper;
+import org.cogchar.app.puma.config.PumaContextMediator;
+import org.cogchar.app.puma.config.PumaModeConstants;
+import org.cogchar.blob.emit.OnlineSheetRepoSpec;
+import org.cogchar.blob.emit.RepoSpec;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends BundleActivatorBase {
+
+	public static Dataset theMainConfigDataset;
+	
+	public void start(BundleContext context) throws Exception {
+		forceLog4jConfig();
+		initWebapps(context);
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// TODO add deactivation code here
+	}
+
+	public void initWebapps(BundleContext context) {
+		// Since we are not running PumaBooter, we must at least start the query service to get sheet-based config going
+		PumaContextMediator mediator = new RepoPumaMediator();
+		String roleShortName = "pumaCtx_FrienduRepo";
+		Ident ctxID = new FreeIdent(PumaModeConstants.RKRT_NS_PREFIX + roleShortName, roleShortName);		
+		PumaAppContext pac = new PumaAppContext(context, mediator, ctxID);
+		pac.startRepositoryConfigServices();
+		// ... and set our app context with PumaWebMapper, so lift can issue repo update requests
+		PumaWebMapper pwm = pac.getOrMakeWebMapper();	
+		pwm.connectLiftInterface(context);
+		// Tell the lifter lifecycle to start, once its OSGi dependencies are satisfied
+		pwm.startLifterLifecycle(context);
+	}
+	
+	private static class RepoPumaMediator extends PumaContextMediator {
+		// Override base class methods to customize the way that PUMA boots + runs, and
+		// to receive notifications of progress during the boot / re-boot process.
+		String TEST_REPO_SHEET_KEY = "0ArBjkBoH40tndDdsVEVHZXhVRHFETTB5MGhGcWFmeGc"; // Main Repo
+		int  DFLT_NAMESPACE_SHEET_NUM = 9;
+		int   DFLT_DIRECTORY_SHEET_NUM = 8;
+		
+		@Override public RepoSpec getMainConfigRepoSpec() {
+			java.util.List<ClassLoader> fileResModelCLs = new java.util.ArrayList<ClassLoader>();
+			return new OnlineSheetRepoSpec(TEST_REPO_SHEET_KEY, DFLT_NAMESPACE_SHEET_NUM, DFLT_DIRECTORY_SHEET_NUM,
+							fileResModelCLs);
+		}
+	}
+}
