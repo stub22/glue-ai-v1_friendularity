7ea35536840589ae98556e707d9a9f26841c05d2
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2013-03-13 09:48:20 +0000

    Added Joseki and Goody-Pump bindings, so that o.f.b.demo.liftoff can now display goodies requested by a Robosteps client (e.g. Robosteps "VirtualWorldTest").

--- trunk/maven/org.friendularity.bundle.demo.liftoff/src/main/java/org/friendularity/bundle/demo/liftoff/Activator.java
+++ trunk/maven/org.friendularity.bundle.demo.liftoff/src/main/java/org/friendularity/bundle/demo/liftoff/Activator.java
@@ -1,14 +1,18 @@
 package org.friendularity.bundle.demo.liftoff;
 
+import org.appdapter.osgi.core.BundleActivatorBase;
 import org.cogchar.bind.lift.LifterLifecycle;
+import org.cogchar.bundle.app.puma.PumaAppUtils;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.robokind.api.common.osgi.lifecycle.OSGiComponent;
+import org.cogchar.joswrap.RepoUpdateCallbackAdapter;
 
-public class Activator implements BundleActivator {
+public class Activator extends BundleActivatorBase {
 
     public void start(BundleContext context) throws Exception {
         initWebapp(context);
+		setupRepoUpdateCallback();
     }
 
     public void stop(BundleContext context) throws Exception {
@@ -21,4 +25,18 @@ public class Activator implements BundleActivator {
         OSGiComponent lifterComp = new OSGiComponent(context, lifecycle);
         lifterComp.start();
     }
+	
+	public void setupRepoUpdateCallback() {
+		/**
+		 * This is a giant hack around the topic of properly notifying our VWorld (or any other aspect of Cogchar)
+		 * that something in our "main memory" repo has changed.  At present we assume that any such update means
+		 * we should simply update the Goodies in our V-World.
+		 */
+		RepoUpdateCallbackAdapter.registerCallback(new RepoUpdateCallbackAdapter.Callback() {
+			public void repoUpdateCompleted() {
+				getLogger().info("o.f.b.demo.liftoff activator got SPARQL-UPDATE callback, now pumping Goody Updates!");
+				PumaAppUtils.pumpGoodyUpdatesToVWorld();
+			}
+		});
+	}
 }
