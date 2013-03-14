e965053c8ebdc46b510672abd6a948b0f2c69b18
jgpallack <jgpallack@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2013-03-14 21:47:06 +0000

    Demo app for org.robokind.basic Basic API

---
+++ trunk/rkbasic_demo/src/main/java/rkbasic/demo/App.java
@@ -0,0 +1,46 @@
+package rkbasic.demo;
+
+import org.robokind.api.animation.Animation;
+import org.robokind.api.animation.messaging.RemoteAnimationPlayerClient;
+import org.robokind.api.animation.player.AnimationJob;
+import org.robokind.api.common.position.NormalizedDouble;
+import org.robokind.api.motion.Joint;
+import org.robokind.api.motion.messaging.RemoteRobot;
+import org.robokind.api.speech.messaging.RemoteSpeechServiceClient;
+import org.robokind.basic.Robokind;
+
+import static org.robokind.api.motion.Robot.*;
+import static org.robokind.basic.RobotJoints.*;
+
+public class App {
+    private static RemoteRobot myRobot;
+    private static RemoteAnimationPlayerClient myPlayer;
+    private static RemoteSpeechServiceClient mySpeaker;
+    private static RobotPositionMap myGoalPositions;
+    
+    public static void main( String[] args ){
+        long animLen;
+        
+        myRobot = Robokind.connectRobot();
+        myPlayer = Robokind.connectAnimationPlayer();
+        mySpeaker = Robokind.connectSpeechService();
+        myGoalPositions = new RobotPositionHashMap();
+        
+        JointId jointId = new JointId(myRobot.getRobotId(), new Joint.Id(WAIST));
+
+        myGoalPositions.put(jointId, new NormalizedDouble(0.0));
+        myGoalPositions.put(jointId, new NormalizedDouble(1.0));
+        myGoalPositions.put(jointId, new NormalizedDouble(0.5));
+        
+        myRobot.move(myGoalPositions, 1000);
+        
+        Animation introAnim = Robokind.loadAnimation("intro.anim.xml");
+        AnimationJob introJob = myPlayer.playAnimation(introAnim);
+        animLen = introAnim.getLength();
+        mySpeaker.speak("Hello, my name is ZENO.");
+        Robokind.sleep(500 + animLen);
+        
+        Robokind.disconnect();
+        System.exit(0);
+    }
+}
