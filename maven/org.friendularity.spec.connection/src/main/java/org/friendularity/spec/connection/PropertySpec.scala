2039a4d5277f5e131e765a26a465c9b8a04de185
Jason Randolph Eads <jeads442@gmail.com>
2013-07-24 21:03:49 +0000

    Adding and updating Specs and SpecBuilders for ServiceBinding and Property

--- maven/org.friendularity.spec.connection/src/main/java/org/friendularity/spec/connection/PropertySpec.scala
+++ maven/org.friendularity.spec.connection/src/main/java/org/friendularity/spec/connection/PropertySpec.scala
@@ -18,6 +18,7 @@ package org.friendularity.spec.connection
 import org.appdapter.core.component.KnownComponentImpl
 import org.appdapter.bind.rdf.jena.assembly.CachingComponentAssembler
 import org.appdapter.core.item.{Item}
+import org.appdapter.core.name.{Ident}
 import com.hp.hpl.jena.assembler.{Assembler, Mode}
 
 /**
@@ -47,6 +48,11 @@ class PropertySpec extends KnownComponentImpl {
  * @author Jason Randolph Eads <eadsjr@hansonrobokind.com>
  */
 class PropertySpecBuilder extends CachingComponentAssembler[PropertySpec] {
+  
+  override protected def decideComponentClass(ident: Ident, item: Item): Class[PropertySpec] = {
+    return classOf[PropertySpec]
+  }
+  
   override protected def initExtendedFieldsAndLinks(pSpec: PropertySpec, configItem : Item, assmblr : Assembler , mode: Mode ) {
     val itemAssemblyReader = getReader();
     
