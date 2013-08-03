08bec988b82e2e1f5cd87531334aa858d06400c1
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2013-08-03 21:21:38 +0000

    OpenCV 2.4.6 java code + tests, plus Anne's demo program, wrapped in an OSGi bundle. Checking in all DLLs for WinX64 - including the 9MB ffmpeg. Data files such as the haarcascades config.xml are not yet included.

---
+++ trunk/maven/org.friendularity.bundle.jvision/src/main/java/org/friendularity/jvision/filters/Dilate.java
@@ -0,0 +1,18 @@
+package org.friendularity.jvision.filters;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+/*
+ * blur with kernal size 5
+ */
+public class Dilate implements BaseFilter {
+	private static Size kernal_size = new Size(13, 13);
+	@Override
+	public void apply(Mat in, Mat out) {
+		Mat kernal = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, kernal_size);
+		
+		Imgproc.dilate(in, out, kernal);
+	}
+
+}
---
+++ trunk/maven/org.friendularity.bundle.jvision/src/main/java/org/friendularity/jvision/gui/FileLocations.java
@@ -0,0 +1,17 @@
+package org.friendularity.jvision.gui;
+
+/*
+ * Tacky method of making it easier to move things about
+ * 
+ * presumably does something more sophisticated someday
+ * 
+ */
+public class FileLocations {
+	public static final String imageBase() {
+		return "E:\\_mount\\_test\\anne_vision_2013\\nbhe\\";
+	}
+	
+	public static final String lbpCascadeBase() {
+		return "E:\\_mount\\_test\\anne_vision_2013\\data_patched\\lbpcascades\\";
+	}	
+}
