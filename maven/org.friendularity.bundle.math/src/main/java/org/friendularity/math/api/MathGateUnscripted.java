f05c7dcf7cfdaa0bf93309617cd639a86a113a3f
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2013-09-03 17:22:55 +0000

    Math history-list fix absorbed into MathGateUnscripted.java, also starting more detailed anim workouts for Sinbad.

--- maven/org.friendularity.bundle.math/src/main/java/org/friendularity/math/api/MathGateUnscripted.java
+++ maven/org.friendularity.bundle.math/src/main/java/org/friendularity/math/api/MathGateUnscripted.java
@@ -15,6 +15,8 @@
  */
 package org.friendularity.math.api;
 
+// Requires  classes from  guava.jar, which are not currently exported by ext.bundle.math.symja_jas
+//import com.google.common.base.Predicate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,6 +27,7 @@ import org.matheclipse.core.convert.Object2Expr;
 import org.matheclipse.core.eval.EvalEngine;
 import org.matheclipse.core.eval.EvalUtilities;
 import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.ISymbol;
 
@@ -32,11 +35,13 @@ import org.matheclipse.core.interfaces.ISymbol;
  *
  * @author Stu B22 <stub22@appstract.com>
  *
- * Uses code + comments copied from symja's "MathScriptEngine"
+ * Uses code + comments copied from Symja's "MathScriptEngine"
  */
 public class MathGateUnscripted extends MathGate {
 	private EvalEngine	 myEvalEngine;
-	private EvalUtilities myEvalUtilityWrapper;
+	// We are dropping EvalUtilities since it likes to keep a cache that grows in memories.
+	// But we copy in the 
+	//	private EvalUtilities myEvalUtilityWrapper;
 	private Map<String, Object> myVarBindings = new HashMap<String, Object>();
 	
 	private	Map<String,	IExpr> myParsedExprCache = new HashMap<String, IExpr>();
@@ -46,7 +51,7 @@ public class MathGateUnscripted extends MathGate {
 		myEvalEngine = new EvalEngine();
 		// engine.setIterationLimit(10);
 		// The "false" controls some MathML prefixing behavior that we probably don't care about 
-		myEvalUtilityWrapper = new EvalUtilities(myEvalEngine, false);
+		// myEvalUtilityWrapper = new EvalUtilities(myEvalEngine, false);
 	}
 
 	@Override public void putVar(String name, Object var) {
@@ -64,7 +69,8 @@ public class MathGateUnscripted extends MathGate {
 	// * Assign the associated EvalEngine to the current thread. Every subsequent
 	// * action evaluation in this thread affects the EvalEngine in this class.		
 		
-		myEvalUtilityWrapper.startRequest();
+		// myEvalUtilityWrapper.startRequest();
+		evalEngineAttach();
 		
 		for (Map.Entry<String, Object> currEntry : myVarBindings.entrySet()) {
 			ISymbol symbol = F.$s(currEntry.getKey());
@@ -105,7 +111,8 @@ public class MathGateUnscripted extends MathGate {
 			//	if (Boolean.TRUE.equals(stepwise)) {
 			//		result = fUtility.evalTrace(script, null, F.List());
 			//	} else {
-					resultExpr = myEvalUtilityWrapper.evaluate(cachedExpr);
+					// resultExpr = myEvalUtilityWrapper.evaluate(cachedExpr);
+					resultExpr = evalParsedExpr(cachedExpr);
 
 			} else {
 				getLogger().error("Cannot parse expr [" + exprText + "]");
@@ -122,4 +129,90 @@ public class MathGateUnscripted extends MathGate {
 		return resultExpr;
 	}
 
+		/** Copied from MathMLUtilities.startRequest() - the parent class of EvalUtilities:
+	 * Assign the associated EvalEngine to the current thread. Every subsequent
+	 * action evaluation in this thread affects the EvalEngine in this class.
+	 */
+	public void evalEngineAttach() {
+		EvalEngine.set(myEvalEngine);
+	}
+
+	/** Copied from MathMLUtilities.stopRequest() - the parent class of EvalUtilities:
+	 * Stop the current evaluation thread, by setting stopRequested to true in the EvalEngine.
+	 * Actual impacts are outside EvalEngine, apparently.  Speculation:  Seems to be "interrupt 
+	 * - stop trying to solve", and perhaps also triggers some explicit cleanup? 
+	 */
+	public void evalEngineStop() {
+		myEvalEngine.stopRequest();
+	}
+	
+	public IExpr evalMathText(final String inTextExpr) throws Exception {
+		IExpr parsedExpression = null;
+		if (inTextExpr != null) {
+			evalEngineAttach();
+			myEvalEngine.reset();
+			parsedExpression = myEvalEngine.parse(inTextExpr);
+			if (parsedExpression != null) {
+				myEvalEngine.reset();
+				IExpr temp = myEvalEngine.evaluate(parsedExpression);
+				// Stu B22 - Commented out this line as recommended by Axel.
+				// It is keeping a history of values for future access via the Out[] function.
+				// But this causes RAM usage to grow over time!
+				// fEvalEngine.addOut(temp);
+				return temp;
+			}
+		}
+		return null;
+	}
+
+	public IExpr evalParsedExpr(final IExpr parsedExpr) throws RuntimeException {
+		if (parsedExpr != null) {
+			evalEngineAttach();
+			myEvalEngine.reset();
+			IExpr temp = myEvalEngine.evaluate(parsedExpr);
+			// Stu B22 - Commented out this line as recommended by Axel.
+			// It is keeping a history of values for future access via the Out[] function.
+			// But this causes RAM usage to grow over time! myEvalEngine.addOut(temp);
+			return temp;
+		}
+		return null;
+	}	
+	/**
+	 * Evaluate the <code>inputExpression</code> and return the
+	 * <code>Trace[inputExpression]</code> (i.e. all (sub-)expressions needed to
+	 * calculate the result).
+	 * 
+	 * @param inputExpression
+	 *          the expression which should be evaluated.
+	 * @param matcher
+	 *          a filter which determines the expressions which should be traced,
+	 *          If the matcher is set to <code>null</code>, all expressions are
+	 *          traced.
+	 * @param list
+	 *          an IAST object which will be cloned for containing the traced
+	 *          expressions. Typically a <code>F.List()</code> will be used.
+	 * @return
+	 */
+	// Disabling because it requires Guava classes - need to add them as export from ext.bundle.math.symja_jas
+	/*
+	public IAST traceEvalMathText(final String inputExpression, com.google.common.base.Predicate<IExpr> matcher, IAST list) throws Exception {
+		IExpr parsedExpression = null;
+		if (inputExpression != null) {
+			// try {
+			evalEngineAttach();
+			myEvalEngine.reset();
+			parsedExpression = myEvalEngine.parse(inputExpression);
+			if (parsedExpression != null) {
+				myEvalEngine.reset();
+				IAST temp = myEvalEngine.evalTrace(parsedExpression, matcher, list);
+				// Stu B22 - Commented out this line as recommended by Axel.
+				// It is keeping a history of values for future access via the Out[] function.
+				// But this causes RAM usage to grow over time!
+				// fEvalEngine.addOut(temp);
+				return temp;
+			}
+		}
+		return null;
+	}	
+	*/
 }
