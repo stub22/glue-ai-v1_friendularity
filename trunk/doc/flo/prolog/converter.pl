8a2b5fae325b04e591033b0a6e31a3c305c1f18e
Anniepoo <Anniepoo@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2013-09-08 20:02:28 +0000

    bug fix, outputs no longer listed as inputs

--- trunk/doc/flo/prolog/converter.pl
+++ trunk/doc/flo/prolog/converter.pl
@@ -7,11 +7,52 @@
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(semweb/rdf_turtle_write)).
 :- use_module(library(semweb/rdfs)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/http_open)).
+% :- use_module(library(http/http_ssl_plugin)).
 
 :- rdf_register_prefix(flo, 'http://www.friendularity.org/ontology/flo#').
 
 
 :- rdf_meta assert_rdf_list(o, r, r, +, r, +).
+
+/*
+how to get csv from googledocs
+
+https://docs.google.com/spreadsheet/ccc?key=0AkhpumfMtW6DdE5adlZjeHk4OWZwMGhGM1p3SzNYWFE&output=csv&range=A1:AC99
+*/
+:- use_module(library(http/http_open)).
+:- use_module(library(http/http_ssl_plugin)).
+
+cert_verify(_SSL, _ProblemCert, _AllCerts, _FirstCert, _Error) :-
+        format(user_error, 'Accepting certificate~n', []).
+
+google_proto_ss('https://docs.google.com/spreadsheet/ccc?key=0AkhpumfMtW6DdE5adlZjeHk4OWZwMGhGM1p3SzNYWFE&output=csv&range=A1:AC99').
+
+google_unsecure_proto_ss('http://docs.google.com/spreadsheet/ccc?key=0AkhpumfMtW6DdE5adlZjeHk4OWZwMGhGM1p3SzNYWFE&output=csv&range=A1:AC99').
+
+get_google_sheet :-
+	google_proto_ss(HTTPS_url),
+        http_open(HTTPS_url, In,
+                  [ cert_verify_hook(cert_verify)
+                  ]),
+	read_stream_to_codes(In, Codes),
+	close(In),
+	format('~s', [Codes]),
+	open('fromgoogle.csv', write, Out, [encoding(octet)]),
+	format(Out, '~s', [Codes]),
+	close(Out).
+
+unsecure_get_google_sheet :-
+	google_unsecure_proto_ss(HTTP_url),
+        http_open(HTTP_url, In, [cert_verify_hook(cert_verify)]),
+	read_stream_to_codes(In, Codes),
+	close(In),
+	format('~s', [Codes]),
+	open('fromgoogle.csv', write, Out, [encoding(octet)]),
+	format(Out, '~s', [Codes]),
+	close(Out).
+
 
 
 convert(Outfile) :-
@@ -57,6 +98,7 @@ add_rdf([H|T]) :-
 assert_rdf_list(_, _, _, [], _, _).
 assert_rdf_list(Subj, PredLinkingSubj, Type1, [required(H1)|T1], Type2, [H2|T2]) :- !,
 	rdf_bnode(BNode),
+	rdf_assert(BNode, rdf:type, flo:'BlockInput'),
 	rdf_assert(BNode, PredLinkingSubj, Subj),
 	rdf_assert(BNode, Type1, literal(H1)),
 	rdf_assert(BNode, Type2, literal(H2)),
@@ -64,6 +106,7 @@ assert_rdf_list(Subj, PredLinkingSubj, Type1, [required(H1)|T1], Type2, [H2|T2])
 	assert_rdf_list(Subj, PredLinkingSubj, Type1, T1, Type2, T2).
 assert_rdf_list(Subj, PredLinkingSubj, Type1, [optional(H1)|T1], Type2, [H2|T2]) :- !,
 	rdf_bnode(BNode),
+	rdf_assert(BNode, rdf:type, flo:'BlockInput'),
 	rdf_assert(BNode, PredLinkingSubj, Subj),
 	rdf_assert(BNode, Type1, literal(H1)),
 	rdf_assert(BNode, Type2, literal(H2)),
@@ -71,10 +114,11 @@ assert_rdf_list(Subj, PredLinkingSubj, Type1, [optional(H1)|T1], Type2, [H2|T2])
 	assert_rdf_list(Subj, PredLinkingSubj, Type1, T1, Type2, T2).
 assert_rdf_list(Subj, PredLinkingSubj, Type1, [H1|T1], Type2, [H2|T2]) :-
 	rdf_bnode(BNode),
+	rdf_assert(BNode, rdf:type, flo:'BlockOutput'),
 	rdf_assert(BNode, PredLinkingSubj, Subj),
 	rdf_assert(BNode, Type1, literal(H1)),
 	rdf_assert(BNode, Type2, literal(H2)),
-	rdf_assert(Subj, flo:optionalInput, BNode),
+	rdf_assert(Subj, flo:hasOutput, BNode),
 	assert_rdf_list(Subj, PredLinkingSubj, Type1, T1, Type2, T2).
 
 
