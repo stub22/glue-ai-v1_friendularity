4e859a63f928e92a2ae5194be63e0d2ee76ab7a9
Anne Ogborn <annie66us@yahoo.com>
2013-09-28 22:46:47 +0000

    splitting starting to come up. can split hor2 a single time (not handling parent is jsplitpane yet)

--- maven/org.friendularity.bundle.bento/src/main/java/org/friendularity/bundle/bento/gui/CameraViewer.java
+++ maven/org.friendularity.bundle.bento/src/main/java/org/friendularity/bundle/bento/gui/CameraViewer.java
@@ -18,7 +18,9 @@ package org.friendularity.bundle.bento.gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import javax.swing.JSplitPane;
 
 import org.friendularity.jvision.engine.Displayer;
 import org.friendularity.jvision.engine.JVisionEngine;
@@ -37,22 +39,31 @@ class CameraViewer extends BentoPlugin  implements Displayer {
 		super();
 		
 		this.setPreferredSize(new Dimension(640, 480));
+		this.setMinimumSize(new Dimension(160, 120));
 		JVisionEngine.getDefaultJVisionEngine().addDisplayer(this);
 	}
+	
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g); 
 		
 		if(mImage != null)
-			g.drawImage(mImage, 0, 0, this);
+		{
+			Rectangle destRect = new Rectangle();
+			bestAntiAliasedFit(this.getVisibleRect(), mImage.getWidth(), mImage.getHeight(), destRect);
+			g.drawImage(mImage, destRect.x, destRect.y, destRect.width, destRect.height, this);
+		}
 		else if (++noImage % 1000 == 0)
 		{
 			System.err.println("no image");
 		}
-		g.setColor(Color.red);
-		g.drawLine(0, 0, 100, 100);
-		
+		/*
+		g.setColor(Color.red);	
+		g.drawString("size: " + this.getSize().toString(), 200, 50);
+		g.drawString("loc: " + this.getLocation().toString(), 200, 75);
+		g.drawString("clip: " + this.getVisibleRect().toString(), 200, 100);
+		*/
 		// mFrameMessage has a valid message
 	}
 
@@ -66,5 +77,31 @@ class CameraViewer extends BentoPlugin  implements Displayer {
 	@Override
 	public void setFramerateMessage(String string) {
 		mFrameMessage = string;
+	}
+
+	/**
+	 *  Lay out an image in the given area so it shows the best location
+	 *  The best location is the size of the layout area or smaller, the right aspect ratio, and 
+	 *  a small numbered divisor or multiplicand of the original size, and centered in gthe layout area
+	 * 
+	 * @param layoutArea  rectangle to lay out in
+	 * @param width image width
+	 * @param height image height
+	 * @param destRect   location to draw into
+	 */
+	private static void bestAntiAliasedFit(Rectangle layoutArea, int width, int height, Rectangle destRect) {
+		int divisor = 1;
+		
+		for( ; divisor < 2 << 16; divisor = divisor << 1)
+		{
+			if (layoutArea.width >= 256 * width / divisor && layoutArea.height >= 256 * height / divisor)
+				break;
+		}
+		
+		destRect.width = 256 * width / divisor;
+		destRect.height = 256 * height / divisor;
+		
+		destRect.x = layoutArea.x + layoutArea.width / 2 - destRect.width / 2;
+		destRect.y = layoutArea.y + layoutArea.height / 2 - destRect.height / 2;
 	}
 }
