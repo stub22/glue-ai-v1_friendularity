7548d0e97269c5325efb19e6f586f2f7b1a971c7
Anniepoo <Anniepoo@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2013-09-28 06:40:25 +0000

    right menus appearing partially

--- trunk/maven/org.friendularity.bundle.bento/src/main/java/org/friendularity/bundle/bento/gui/BentoPlugin.java
+++ trunk/maven/org.friendularity.bundle.bento/src/main/java/org/friendularity/bundle/bento/gui/BentoPlugin.java
@@ -15,16 +15,118 @@
  */
 package org.friendularity.bundle.bento.gui;
 
-import javax.swing.JComponent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.KeyStroke;
+import org.friendularity.bundle.bento.util.Bento_OSGi_ResourceLoader;
 
 /**
  *
  * @author Annie
  */
-class BentoPlugin  extends JPanel {
-
+class BentoPlugin  extends JPanel implements ActionListener {
 	public BentoPlugin() {
+		try {
+			initPopup();
+		} catch (IOException ex) {
+			Logger.getLogger(BentoPlugin.class.getName()).log(Level.SEVERE, null, "missing resources probably wont come up");
+		}
 	}
 	
+	protected void handleActions(ActionEvent e)
+	{
+		System.err.println(e.getActionCommand());
+	}
+	
+	/* ================= Popup menu handling ================ */
+	
+	/**
+	 * Add additional menu items at startup'
+	 * Safe to do this on any thread
+	 * 
+	 * @param menu 
+	 */
+	protected void addAdditionalMenuItems(JPopupMenu menu)
+	{
+		
+	}
+	
+	/**
+	 * Override only to completely replace popup menu
+	 * 
+	 * you probably want addAdditionalMenuItems
+	 * 
+	 * 
+	 */
+	protected void initPopup() throws IOException
+	{
+		
+		ImageIcon ii = new ImageIcon(
+				Bento_OSGi_ResourceLoader.getDefaultImageLoader().getImageResource("/img/twohor.png"));
+		
+		popup = new JPopupMenu();
+		JMenuItem menuItem = new JMenuItem("Split Horizontal", ii);
+		menuItem.addActionListener(this);
+		popup.add(menuItem);
+		
+		ii = new ImageIcon(
+				Bento_OSGi_ResourceLoader.getDefaultImageLoader().getImageResource("/img/threehor.png"));
+		menuItem = new JMenuItem("Split 3 Horizontal", ii);
+		menuItem.addActionListener(this);
+		popup.add(menuItem);
+		
+		addAdditionalMenuItems(popup);
+
+		//Add listener to components that can bring up popup menus.
+		MouseListener popupListener = new PopupListener();
+		this.addMouseListener(popupListener);
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		handleActions(e);
+	}
+	
+   private JPopupMenu popup;
+   
+   /**
+	* get the right click menu
+	* 
+	* If you modify it, do it on swingworker thread
+	* 
+	* @return popup menu
+	*/
+   protected JPopupMenu getPopup()
+   {
+	   return popup;
+   }
+	
+	class PopupListener extends MouseAdapter {
+		public void mousePressed(MouseEvent e) {
+			maybeShowPopup(e);
+		}
+
+		public void mouseReleased(MouseEvent e) {
+			maybeShowPopup(e);
+		}
+
+		private void maybeShowPopup(MouseEvent e) {
+			if (e.isPopupTrigger()) {
+				popup.show(e.getComponent(),
+						   e.getX(), e.getY());
+			}
+		}
+	}
 }
