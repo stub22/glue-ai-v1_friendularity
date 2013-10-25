ef9d83b14b9e357c875a0fe0c9ff5e51f859fbf9
mjacquote <mjacquote@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2013-10-25 18:47:56 +0000

    Will change again later.  Added files for automatic spec registration.

--- maven/org.friendularity.connection/src/main/java/org/friendularity/connection/Activator.java
+++ maven/org.friendularity.connection/src/main/java/org/friendularity/connection/Activator.java
@@ -1,7 +1,7 @@
-
 package org.friendularity.connection;
 
 import java.util.List;
+
 import javax.swing.UIManager;
 // import org.cogchar.outer.behav.demo.RepoConnector;
 import org.osgi.framework.BundleActivator;
@@ -14,20 +14,20 @@ import org.appdapter.core.matdat.EnhancedRepoClient;
 import org.appdapter.core.store.Repo;
 import org.cogchar.impl.scene.read.BehavMasterConfigTest;
 
+import org.friendularity.spec.connection.RegisterWiring;
 import org.cogchar.platform.util.ClassLoaderUtils;
-import org.cogchar.bundle.temp.wire.ServiceManagerWiring;
+import org.friendularity.spec.connection.ServiceManagerWiring;
 import org.rwshop.swing.common.lifecycle.ServicesFrame;
 
 /**
- * Bundle Activator for the demo connection package. This project serves as an 
- * example use-case for the glueAI data flow system. Information is pulled from
- * repositories, in this case a Google spreadsheet, and is converted to
- * in-system information.
- * 
+ * Bundle Activator for the demo connection package. This project serves as an example use-case for
+ * the glueAI data flow system. Information is pulled from repositories, in this case a Google
+ * spreadsheet, and is converted to in-system information.
+ *
  * @author Yishuai Li & Jason Eads
  */
 public class Activator implements BundleActivator {
-    
+
 //    private final static String CONNECTION_GRAPH_QN =
 //            "fc:connection_sheet_1";
     private final static String SERVICE_BINDING_GRAPH_QN =
@@ -37,49 +37,59 @@ public class Activator implements BundleActivator {
 //    private final static String PIPELINE_QN = "csi:pipeline_sheet_77";
     private final static String MERGED_MODEL_MANAGER_QN =
             "csi:merged_manager_1001";
-    
+    private final static String MERGED_MODEL_MANAGER_QN2 =
+            "csi:merged_manager_1002";
+
     public void start(BundleContext context) throws Exception {
         setLookAndFeel();
         startServicePanel(context);
         // Setup to connect to a GoogSheet repo
-        List<ClassLoader> classloaders = 
+        List<ClassLoader> classloaders =
                 ClassLoaderUtils.getFileResourceClassLoaders(
-                    context,
-                    ClassLoaderUtils.ALL_RESOURCE_CLASSLOADER_TYPES);
-        OnlineSheetRepoSpec repoSpec = 
-//                //GluePuma_BehavMasterDemo
-//                new OnlineSheetRepoSpec(
-//                    "0AlpQRNQ-L8QUdFh5YWswSzdYZFJMb1N6aEhJVWwtR3c",
-//                    4,
-//                    3,
-//                    classloaders);
+                context,
+                ClassLoaderUtils.ALL_RESOURCE_CLASSLOADER_TYPES);
+        OnlineSheetRepoSpec repoSpec =
+                //                //GluePuma_BehavMasterDemo
+                //                new OnlineSheetRepoSpec(
+                //                    "0AlpQRNQ-L8QUdFh5YWswSzdYZFJMb1N6aEhJVWwtR3c",
+                //                    4,
+                //                    3,
+                //                    classloaders);
                 // GluePuma_HRKR50_TESTFULL
+                //                new OnlineSheetRepoSpec(
+                //                    "0ArBjkBoH40tndDdsVEVHZXhVRHFETTB5MGhGcWFmeGc",
+                //                    9,
+                //                    8,
+                //                    classloaders);
+
                 new OnlineSheetRepoSpec(
-                    "0ArBjkBoH40tndDdsVEVHZXhVRHFETTB5MGhGcWFmeGc",
-                    9,
-                    8,
-                    classloaders);
-		
-       // RepoConnector rc = new RepoConnector();
+                "0AivIV8RvlFTvdGdCZkQ5Q1k1ZnkwMzg4UkZqbmZfa0E",
+                1,
+                12,
+                classloaders);
+        
+        Repo.WithDirectory bmcMemoryRepoHandle = repoSpec.makeRepo();
+        EnhancedRepoClient enhancedRepoSpec = new EnhancedRepoClient(repoSpec, bmcMemoryRepoHandle,
+                BehavMasterConfigTest.TGT_GRAPH_SPARQL_VAR(), BehavMasterConfigTest.QUERY_SOURCE_GRAPH_QN());
+
+        //RepoConnector rc = new RepoConnector();
         //EnhancedRepoClient enhancedRepoSpec =  rc.connectDemoRepoClient(repoSpec);
 
-		Repo.WithDirectory bmcMemoryRepoHandle = repoSpec.makeRepo();
-		EnhancedRepoClient enhancedRepoSpec = new EnhancedRepoClient(repoSpec, bmcMemoryRepoHandle, 
-					BehavMasterConfigTest.TGT_GRAPH_SPARQL_VAR(), BehavMasterConfigTest.QUERY_SOURCE_GRAPH_QN());    
-		
+        RegisterWiring.loadAndRegisterSpec(context, enhancedRepoSpec, MERGED_MODEL_MANAGER_QN);
 
-        
+
         // Load the specs from the repo, and register them with JFlux
         ServiceManagerWiring.loadAndRegisterSpecs(
                 context,
                 enhancedRepoSpec,
                 LIFECYCLE_DEFINITION_GRAPH_QN,
                 SERVICE_BINDING_GRAPH_QN,
-                MERGED_MODEL_MANAGER_QN);
-        
+                MERGED_MODEL_MANAGER_QN2);
+
         //Extender listens for specs and creates objects from them
         ServiceManagerWiring.startSpecExtender(context, null);
     }
+
     private void setLookAndFeel() {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -97,7 +107,8 @@ public class Activator implements BundleActivator {
             }
         });
     }
-    
+
     @Override
-    public void stop(BundleContext context) throws Exception {}
+    public void stop(BundleContext context) throws Exception {
+    }
 }
