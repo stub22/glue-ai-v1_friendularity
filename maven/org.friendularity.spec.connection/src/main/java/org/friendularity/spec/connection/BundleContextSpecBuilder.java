951a221f490593d92f23c4db762fef0300179f50
mjacquote <mjacquote@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2013-12-03 09:26:14 +0000

    Added Code for ServicePanel to be launchable from spreadsheets. The logger now displays more information while running the connection project.

---
+++ maven/org.friendularity.spec.connection/src/main/java/org/friendularity/spec/connection/BundleContextSpecBuilder.java
@@ -0,0 +1,38 @@
+package org.friendularity.spec.connection;
+
+import org.appdapter.bind.rdf.jena.assembly.CachingComponentAssembler;
+import org.appdapter.core.item.Item;
+import org.appdapter.core.name.Ident;
+
+import com.hp.hpl.jena.assembler.Assembler;
+import com.hp.hpl.jena.assembler.Mode;
+
+import com.hp.hpl.jena.assembler.assemblers.AssemblerBase;
+import com.hp.hpl.jena.rdf.model.Resource;
+import org.appdapter.bind.rdf.jena.assembly.ItemAssemblyReader;
+
+/**
+ *
+ * @author Major Jacquote II <mjacquote@gmail.com>
+ */
+public class BundleContextSpecBuilder extends CachingComponentAssembler<BundleContextSpec> {
+    private final static String id="http://www.friedularity.org/Connection#libraryID";
+            
+    public BundleContextSpecBuilder() {
+    }
+
+    @Override
+    protected Class<BundleContextSpec> decideComponentClass(Ident ident, Item item) {
+        return BundleContextSpec.class;
+    }
+
+    @Override
+    protected void initExtendedFieldsAndLinks(
+            BundleContextSpec bundleContextSpec,
+            Item item,
+            Assembler asmblr,
+            Mode mode) {
+
+        
+    }
+}
