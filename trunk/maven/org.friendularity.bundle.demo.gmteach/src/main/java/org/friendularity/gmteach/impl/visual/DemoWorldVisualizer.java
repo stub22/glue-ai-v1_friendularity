cbdabc9289f2f5cb72debb802626cbae84e4fa46
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2013-12-29 19:46:05 +0000

    Tracking Cogchar MIDI changes

--- trunk/maven/org.friendularity.bundle.demo.gmteach/src/main/java/org/friendularity/gmteach/impl/visual/DemoWorldVisualizer.java
+++ trunk/maven/org.friendularity.bundle.demo.gmteach/src/main/java/org/friendularity/gmteach/impl/visual/DemoWorldVisualizer.java
@@ -27,7 +27,7 @@ import org.friendularity.gmteach.api.west.WorldEstimate;
 import org.friendularity.gmteach.vworld.MeshTest;
 import org.cogchar.render.trial.TrialContent;
 import org.cogchar.render.trial.TrialCameras;
-import org.cogchar.render.trial.TempMidiBridge;
+import org.cogchar.bind.midi.in.TempMidiBridge;
 import org.cogchar.render.opengl.optic.ViewportFacade;
 import org.appdapter.core.name.Ident;
 import org.appdapter.core.name.FreeIdent;
