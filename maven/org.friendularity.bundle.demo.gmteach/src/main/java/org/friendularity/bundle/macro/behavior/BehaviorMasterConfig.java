deea926467bbd739f165efb4e57e7f4083f221f0
Douglas R. Miles <logicmoo@gmail.com>
2014-06-06 21:28:59 +0000

    OfflineXlsSheetRepoSpec moved to matdat

--- maven/org.friendularity.bundle.demo.gmteach/src/main/java/org/friendularity/bundle/macro/behavior/BehaviorMasterConfig.java
+++ maven/org.friendularity.bundle.demo.gmteach/src/main/java/org/friendularity/bundle/macro/behavior/BehaviorMasterConfig.java
@@ -6,7 +6,7 @@ package org.friendularity.bundle.macro.behavior;
 
 import java.util.List;
 
-import org.appdapter.core.repo.OfflineXlsSheetRepoSpec;
+import org.appdapter.core.matdat.OfflineXlsSheetRepoSpec;
 import org.appdapter.core.matdat.OnlineSheetRepoSpec;
 import org.appdapter.core.repo.URLRepoSpec;
 import org.cogchar.platform.util.ClassLoaderUtils;
