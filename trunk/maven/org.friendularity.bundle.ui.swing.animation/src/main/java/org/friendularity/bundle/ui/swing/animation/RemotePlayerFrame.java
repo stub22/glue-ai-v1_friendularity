85e29a6d262351bc63aad99c12f06ab7a1885493
krystal.culp <krystal.culp@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2015-06-16 15:57:12 +0000

    Connect anim player to anim converter

--- trunk/maven/org.friendularity.bundle.ui.swing.animation/src/main/java/org/friendularity/bundle/ui/swing/animation/RemotePlayerFrame.java
+++ trunk/maven/org.friendularity.bundle.ui.swing.animation/src/main/java/org/friendularity/bundle/ui/swing/animation/RemotePlayerFrame.java
@@ -72,7 +72,7 @@ public class RemotePlayerFrame extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -100,6 +100,13 @@ public class RemotePlayerFrame extends javax.swing.JFrame {
             }
         });
     }
+
+
+	public DemoPlayerPanel getDemoPlayerPanel() {
+		return demoPlayerPanel1;
+	}
+	
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private org.friendularity.bundle.ui.swing.animation.DemoPlayerPanel demoPlayerPanel1;
     // End of variables declaration//GEN-END:variables
