09346b68ebdcbcf3932ca398c66509d5e87080ab
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2016-03-29 20:14:11 +0000

    Restored QPid test code, and retested with avro's netty dep upgraded to 3.8.0.

--- maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/appro/ApproRaizLifecycle.scala
+++ maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/appro/ApproRaizLifecycle.scala
@@ -174,7 +174,7 @@ import org.osgi.framework.ServiceRegistration
 
 import scala.collection.JavaConversions._
 object ApproRaizDepHelper extends VarargsLogging  {
-/*  Temp commented out on 2016-03-28, because we need ServiceReg deps picked up via JFlux-QPid
+//  Temp commented out on 2016-03-28, because we need ServiceReg deps picked up via JFlux-QPid
 	// These static methods can be used to register services directly, with the property-tags that our ApproRaizLifecycle is waiting for.
 	def regModelSvc_Recipes(ctx : BundleContext, model : JenaModel) = regModelService(ctx, model, ApproRaizLifeLauncher.TAGVAL_MODEL_ROLE_RECIPES)
 	def regModelSvc_Settings(ctx : BundleContext, model : JenaModel) = regModelService(ctx, model, ApproRaizLifeLauncher.TAGVAL_MODEL_ROLE_SETTINGS)
@@ -182,9 +182,8 @@ object ApproRaizDepHelper extends VarargsLogging  {
 
 	def regRcpNmSvc_Main(ctx : BundleContext, rcpNms : ApproRaizRecipeNames) = regRecipeNameService(ctx, rcpNms, ApproRaizLifeLauncher.TAGVAL_RCPN_ROLE_MAIN)
 
-	<!-- Issue with type parameters in OSGi classes, compiled under java 7, v 4.3.0 and 4.3.1
-        http://blog.osgi.org/2012/10/43-companion-code-for-java-7.html
-    -->
+	// Issue with type parameters in OSGi classes, compiled under java 7, v 4.3.0 and 4.3.1
+    //    http://blog.osgi.org/2012/10/43-companion-code-for-java-7.html
 
 	private def regModelService(ctx : BundleContext, model : JenaModel, modelRole : String)  : ServiceRegistration = { // [JenaModel] = {
 		regServiceWithOneProp(ctx, classOf[JenaModel], model, ApproRaizLifeLauncher.TAGKEY_MODEL_ROLE, modelRole)
@@ -202,5 +201,5 @@ object ApproRaizDepHelper extends VarargsLogging  {
 		// ctx.registerService(clz, svc, propDict)
 		ctx.registerService(clz.getName, svc, propDict)
 	}
-*/
+
 }
