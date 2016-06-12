74b5b828fea01f973a2e8833a0530f58077592d4
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2016-06-12 05:28:00 +0000

    Now sending goody thingActs as turtle-string msgs, but receiver side not complete yet.

--- maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/respire/VWGoody.scala
+++ maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/respire/VWGoody.scala
@@ -1,14 +1,32 @@
 package org.friendularity.respire
 
+import java.io.StringReader
+
 import akka.actor.{Actor, ActorContext, ActorRef}
+
+import com.hp.hpl.jena.rdf.model.{Model => JenaModel, ModelFactory => JenaModelFactory}
+import org.appdapter.core.name.{FreeIdent, Ident}
 import org.appdapter.fancy.log.VarargsLogging
 import org.cogchar.impl.thing.basic.BasicThingActionSpec
 import org.cogchar.render.goody.basic.BasicGoodyCtx
+import org.friendularity.dull.ThingActExposer
 
 /**
   * Created by Owner on 5/25/2016.
   */
 trait VWGoodyRqRdf extends VWContentRq  with RdfMsg {
+}
+case class VWGoodyRqTurtle(myTurtleTxt : String) extends VWGoodyRqRdf with VarargsLogging{
+	override def asTurtleString : String = myTurtleTxt
+
+	override def asJenaModel(flags_opt: Option[AnyRef]) : JenaModel = {
+		val modelTurtleTxt : String = asTurtleString
+		val modelReader = new StringReader(modelTurtleTxt)
+		val model = JenaModelFactory.createDefaultModel() ;
+		model.read(modelReader, "TTL")
+		info2("After read, model size is {} bytes, contentDump:\n{}", model.size() : java.lang.Long, model)
+		model
+	}
 }
 trait VWGoodyRqActionSpec extends VWContentRq {
 	def getActionSpec : BasicThingActionSpec
@@ -36,7 +54,17 @@ trait VWGoodyJobLogic extends VarargsLogging {
 
 	protected def processVWGoodyRdfMsg (goodyMsg : VWGoodyRqRdf, localActorCtx : ActorContext) : Unit = {
 		val jenaModel = goodyMsg.asJenaModel(None)
-		// Drawing inspiration from
+
+		// TODO:  Find all URIs of ThingActions, using rdf:type
+		//  @prefix ccrt:  <urn:ftd:cogchar.org:2012:runtime#> .
+		//   ccrt:ThingAction ;
+
+
+		val exposer = new ThingActExposer {}
+
+		// val actionID : Ident = new FreeIdent("wrong_so_fix_me#id")
+		// val actSpec = exposer.readActionFromJenaModel(jenaModel, actionID)
+
 	}
 }
 class VWGoodyActor(myGoodyCtx : BasicGoodyCtx) extends Actor with VWGoodyJobLogic {
