8a229c481ac09914a2823d3329b9892d1bf6446a
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2016-06-16 22:58:30 +0000

    "Ingred" messages are not serializable, only usable within same JVM, and used for bootstrap internal purposes only.

---
+++ maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwmsg/VWIngredMsg.scala
@@ -0,0 +1,25 @@
+package org.friendularity.vwmsg
+
+import org.cogchar.render.sys.context.PhysicalModularRenderContext
+import org.cogchar.render.sys.registry.RenderRegistryClient
+import org.cogchar.render.sys.window.WindowStatusMonitor
+
+// When used in messages, these ingreds are not fully serializable, and thus may not be passed over
+// networking connections.  They generally work OK as messages within a single JVM, however.
+
+trait LesserIngred {
+	// Includes pointers from our JME app that are needed to make a goody space
+	def getRendRegClient : RenderRegistryClient
+	def getWindowStatusMonitor : WindowStatusMonitor
+	// def getGMRendCtx : GoodyModularRenderContext
+}
+trait BodyMgrIngred {
+	def	getPMRC : PhysicalModularRenderContext
+}
+case class FullIngredMsgImpl(rendRegCli : RenderRegistryClient, winStatMon : WindowStatusMonitor, pmrc : PhysicalModularRenderContext)
+			extends LesserIngred with BodyMgrIngred {
+	override def getRendRegClient : RenderRegistryClient = rendRegCli
+	override def getWindowStatusMonitor : WindowStatusMonitor = winStatMon
+	override def	getPMRC : PhysicalModularRenderContext = pmrc
+}
+
