b2a1eb30016823bcd9f87b8e796f10fa16650b46
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2016-06-24 23:32:15 +0000

    Adding instance tracking features to VWShape actor.

--- maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwimpl/VWShaper.scala
+++ maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwimpl/VWShaper.scala
@@ -5,10 +5,13 @@ import com.jme3.material.Material
 import com.jme3.scene.shape.Sphere
 
 import com.jme3.scene.{Node => JmeNode, Geometry, Mesh, Spatial}
+import org.appdapter.core.name.{FreeIdent, Ident}
 import org.appdapter.fancy.log.VarargsLogging
 import org.cogchar.render.sys.registry.RenderRegistryClient
 import org.friendularity.respire.Srtw
-import org.friendularity.vwmsg.{VWSCR_Sphere, VWSCR_TextBox, VWShapeCreateRq, VWSCR_CellGrid, VWStageRqMsg}
+import org.friendularity.vwmsg.{VWCleaAllShapes, VWSCR_Sphere, VWSCR_TextBox, VWShapeCreateRq, VWSCR_CellGrid, VWStageRqMsg}
+
+import scala.collection.mutable
 
 /**
   * Created by Stub22 on 6/22/2016.
@@ -16,6 +19,12 @@ import org.friendularity.vwmsg.{VWSCR_Sphere, VWSCR_TextBox, VWShapeCreateRq, VW
 trait PatternGridMaker extends VarargsLogging {
 	// We are currently calling this *off* rndThrd, and it is working.
 	def makeBigGridAndAttach_onRendThrd(rrc: RenderRegistryClient, parentNode : JmeNode) : Unit = {
+		val ssNode = makeBigGridNode(rrc)
+		info2("Made cellGrid node, attaching: {} to {} - which is supposed to be on rendThrd, but conditionally may " +
+					"work otherwise, too.", ssNode, parentNode)
+		parentNode.attachChild(ssNode) // Question:  Is this the only step that requires us to be onRendThrd?
+	}
+	def makeBigGridNode(rrc: RenderRegistryClient) : JmeNode = {
 		val assetMgr = rrc.getJme3AssetManager(null);
 		val someMat = new Material(assetMgr, "Common/MatDefs/Misc/Unshaded.j3md") // someContent.makeAlphaBlendedUnshadedMaterial(rrc, 0f, 1.0f, 0, 0.5f);
 		val matPal = new MatPallete(someMat)
@@ -36,17 +45,20 @@ trait PatternGridMaker extends VarargsLogging {
 		val chosenIdxs_Z: Range = -5 to 30 by 5
 
 		val ssNode = srtwInst.makeSheetspace(uniformCount, uniformLen, chosenIdxs_X, chosenIdxs_Y, chosenIdxs_Z)
-		info2("Made cellGrid node, attaching: {} to {} - which is supposed to be on rendThrd, but conditionally may " +
-					"work otherwise, too.", ssNode, parentNode)
-		parentNode.attachChild(ssNode) // Question:  Is this the only step that requires us to be onRendThrd?
+		ssNode
+
 	}
 }
-trait VWSpatialsForShapes {
+trait VWSpatialsForShapes extends PatternGridMaker{
+	protected def getTooMuchRRC : RenderRegistryClient
 	//	val tsf: TextSpatialFactory = new TextSpatialFactory(rrc)
 	def makeForRq(vwscr : VWShapeCreateRq) : Spatial = {
 		vwscr match {
 			case txtBox : VWSCR_TextBox => {
 				null
+			}
+			case bigGrid : VWSCR_CellGrid => {
+				makeBigGridNode(getTooMuchRRC)
 			}
 			case meshBased : VWShapeCreateRq => {
 				val mesh: Mesh = meshBased match {
@@ -59,11 +71,11 @@ trait VWSpatialsForShapes {
 				val geom = new Geometry(geomNameArb, mesh)
 				geom
 			}
-
 		}
-
 	}
 }
+
+case class MadeSpatRec(myID : Ident, mySpat : Spatial, myCreateRq : VWShapeCreateRq)
 
 trait VWShaperLogic extends PatternGridMaker {
 	protected def getRRC : RenderRegistryClient
@@ -71,15 +83,55 @@ trait VWShaperLogic extends PatternGridMaker {
 	protected val myTopFlatNode = getRRC.getJme3RootOverlayNode(null)
 	protected val myAssetMgr = getRRC.getJme3AssetManager(null)
 
-	val myShapeMaker = new VWSpatialsForShapes{}
+	val myShapeMaker = new VWSpatialsForShapes{
+		override  protected def getTooMuchRRC : RenderRegistryClient = getRRC
+	}
 
+	lazy protected val myMadeSpatRecsByID = new mutable.HashMap[Ident, MadeSpatRec]()
+
+	protected def clearAllShapes_onRendThrd(): Unit = {
+		for (madeRec <- myMadeSpatRecsByID.values) {
+			val madeSpat = madeRec.mySpat
+			val parentNode = madeSpat.getParent()
+			if (parentNode != null) {
+				parentNode.detachChild(madeSpat)
+			}
+		}
+		myMadeSpatRecsByID.clear()
+	}
 	def makeAndPlace(toMake : VWShapeCreateRq): Unit = {
 		val madeSpat = myShapeMaker.makeForRq(toMake)
+		val kuri_opt = toMake.getKnownURI_opt
+		if (kuri_opt.isDefined) {
+			val knownID = new FreeIdent(kuri_opt.get)
+			val madeSpatRec = new MadeSpatRec(knownID, madeSpat, toMake)
+			val previousMadeSpatRec_opt = myMadeSpatRecsByID.get(knownID)
+			if (toMake.expectUniqueURI && previousMadeSpatRec_opt.isDefined) {
+				warn2("Found existing shape-spat at oldShape={}, so IGNORING newShape={}",
+							previousMadeSpatRec_opt, toMake)
+			} else {
+				myMadeSpatRecsByID.put(knownID, madeSpatRec)
+			}
+		}
+		val knownParentID_opt = toMake.getKnownParentID_opt
+		val knownParentNode_opt : Option[JmeNode] = if (knownParentID_opt.isDefined) {
+			val kparid = knownParentID_opt.get
+			val knownParentMadeRec_opt = myMadeSpatRecsByID.get(kparid)
+			if (knownParentMadeRec_opt.isDefined) {
+				val pNode : JmeNode = knownParentMadeRec_opt.get.mySpat.asInstanceOf[JmeNode]
+				Option(pNode)
+			} else {
+				None
+			}
+		} else None
 
-		if (toMake.inFlatSpace) {
-			myTopFlatNode.attachChild(madeSpat)
-		} else {
-			myTopDeepNode.attachChild(madeSpat)
+		val parentToUse_opt = knownParentNode_opt.orElse(if (toMake.inFlatSpace) {
+				Option(myTopFlatNode)
+			} else {
+			Option(myTopDeepNode)
+		})
+		if (parentToUse_opt.isDefined) {
+			parentToUse_opt.get.attachChild(madeSpat)
 		}
 	}
 	def attachBigGrid(): Unit = {
@@ -91,6 +143,9 @@ class VWShaperActor(myRRC: RenderRegistryClient) extends Actor with VWShaperLogi
 	override protected def getRRC : RenderRegistryClient = myRRC
 
 	def receive = {
+		case clearAll : VWCleaAllShapes => {
+			clearAllShapes_onRendThrd()
+		}
 		case cellGridRq : VWSCR_CellGrid => {
 			attachBigGrid
 		}
--- maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwmsg/VWShapeMsg.scala
+++ maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwmsg/VWShapeMsg.scala
@@ -2,6 +2,7 @@ package org.friendularity.vwmsg
 
 import com.jme3.scene.{Geometry, Mesh, Spatial}
 import com.jme3.scene.shape.Sphere
+import org.appdapter.core.name.Ident
 import org.cogchar.render.trial.TextSpatialFactory
 
 /**
@@ -20,26 +21,35 @@ trait VWShapeCreateRq extends VWContentRq {
 	def getKnownURI_opt : Option[String] = None
 	def expectUniqueURI : Boolean = false
 
+	def getKnownParentID_opt : Option[Ident] = None
 	def inFlatSpace : Boolean = false
 }
 
+trait VWShapeClearRq extends VWContentRq {
+}
+
+case class VWCleaAllShapes()
+
 // These message types are matchable to the fleshier 3D primitives in JMonkey3
 
-case class VWSCR_Sphere(myRadius : Float) extends VWShapeCreateRq
+trait VWMeshyShapeRq extends VWShapeCreateRq
 
-trait VWSCR_Box extends VWShapeCreateRq
+case class VWSCR_Sphere(myRadius : Float) extends VWMeshyShapeRq
 
-trait VWSCR_Cylinder extends VWShapeCreateRq
+trait VWSCR_Box extends VWMeshyShapeRq
 
-trait VWSCR_Torus extends VWShapeCreateRq
+trait VWSCR_Cylinder extends VWMeshyShapeRq
 
-trait VWSCR_PQTorus extends VWShapeCreateRq
+trait VWSCR_Torus extends VWMeshyShapeRq
 
-trait VWSCR_Quad extends VWShapeCreateRq
+trait VWSCR_PQTorus extends VWMeshyShapeRq
 
-trait VWSCR_TextBox extends VWShapeCreateRq {
+trait VWSCR_Quad extends VWMeshyShapeRq
+
+case class  VWSCR_TextBox(contentTxt : String) extends VWShapeCreateRq {
 	override def inFlatSpace : Boolean = true
 }
+
 
 trait VWSCR_Node extends VWShapeCreateRq
 
