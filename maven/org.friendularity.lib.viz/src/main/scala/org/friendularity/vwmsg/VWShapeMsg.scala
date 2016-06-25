0afadb2ac9a6e3b230bd3806635c2526f727b223
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2016-06-25 23:19:47 +0000

    Adding params for loc, rot, color.

--- maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwmsg/VWShapeMsg.scala
+++ maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwmsg/VWShapeMsg.scala
@@ -1,6 +1,6 @@
 package org.friendularity.vwmsg
 
-import com.jme3.math.ColorRGBA
+import com.jme3.math.{Quaternion, Vector3f, ColorRGBA}
 import com.jme3.scene.{Geometry, Mesh, Spatial}
 import com.jme3.scene.shape.Sphere
 import org.appdapter.core.name.Ident
@@ -28,7 +28,35 @@ trait VWShapeCreateRq extends VWContentRq {
 	def inFlatSpace : Boolean = false // Default is inDeepSpace=3D, override to make flat=2D=true
 }
 
+trait Located3D {
+	def getPos : Vector3f
+}
+trait Rotated3D {
+	def getRotQuat : Quaternion
+}
+trait BasicSpatialParams3D extends Located3D with Rotated3D
+
+
+trait Colored {
+	def getColor : ColorRGBA
+}
+
+trait CoreParams3D extends BasicSpatialParams3D with Colored
+
+class OrdinaryParams3D(myPos3f : Vector3f, myRotQuat : Quaternion, myColor : ColorRGBA) extends CoreParams3D {
+	override def getPos : Vector3f = myPos3f
+	override def getRotQuat : Quaternion = myRotQuat
+	override def getColor : ColorRGBA = myColor
+}
+object ParamsConstants {
+	private val  zeroPos = Vector3f.ZERO
+
+	val dullishColor = new ColorRGBA(1.0f, 0.5f, 0.2f, 0.5f);
+	val dullestParams = new OrdinaryParams3D(Vector3f.ZERO, Quaternion.IDENTITY, dullishColor)
+}
+
 trait VWShapeClearRq extends VWContentRq {
+
 }
 
 case class VWClearAllShapes()
@@ -36,11 +64,14 @@ case class VWClearAllShapes()
 // These message types are matchable to the fleshier 3D primitives in JMonkey3
 
 trait VWMeshyShapeRq extends VWShapeCreateRq {
-	def dangerSerialColor : ColorRGBA = new ColorRGBA(1.0f, 0.5f, 0.2f, 0.5f);
+	def getCoreParams3D : Option[CoreParams3D] = None
+	def getColorParam : Option[ColorRGBA] = getCoreParams3D.map(_.getColor)
+	def getPosParam : Option[Vector3f] = getCoreParams3D.map(_.getPos)
+	def getRotParam : Option[Quaternion] = getCoreParams3D.map(_.getRotQuat)
 }
 
-case class VWSCR_Sphere(myRadius : Float, mySerialColor : ColorRGBA) extends VWMeshyShapeRq {
-	override def dangerSerialColor = mySerialColor
+case class VWSCR_Sphere(myRadius : Float, myCoreParams : CoreParams3D) extends VWMeshyShapeRq {
+	override def getCoreParams3D : Option[CoreParams3D] = Option(myCoreParams)
 }
 
 trait VWSCR_Box extends VWMeshyShapeRq
@@ -56,7 +87,6 @@ trait VWSCR_Quad extends VWMeshyShapeRq
 case class  VWSCR_TextBox(contentTxt : String) extends VWShapeCreateRq {
 	override def inFlatSpace : Boolean = true
 }
-
 
 trait VWSCR_Node extends VWShapeCreateRq
 
