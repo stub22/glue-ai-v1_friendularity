568b2506a3e9c486e905b566346decb16b29fc9a
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2016-06-25 21:33:33 +0000

    Refinement of shape attach, now works for Sphere.

--- trunk/maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwimpl/VWShaper.scala
+++ trunk/maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwimpl/VWShaper.scala
@@ -2,6 +2,7 @@ package org.friendularity.vwimpl
 
 import akka.actor.Actor
 import com.jme3.material.Material
+import com.jme3.math.ColorRGBA
 import com.jme3.scene.shape.Sphere
 
 import com.jme3.scene.{Node => JmeNode, Geometry, Mesh, Spatial}
@@ -9,9 +10,10 @@ import org.appdapter.core.name.{FreeIdent, Ident}
 import org.appdapter.fancy.log.VarargsLogging
 import org.cogchar.render.sys.registry.RenderRegistryClient
 import org.friendularity.respire.Srtw
-import org.friendularity.vwmsg.{VWCleaAllShapes, VWSCR_Sphere, VWSCR_TextBox, VWShapeCreateRq, VWSCR_CellGrid, VWStageRqMsg}
+import org.friendularity.vwmsg.{VWMeshyShapeRq, VWClearAllShapes, VWSCR_Sphere, VWSCR_TextBox, VWShapeCreateRq, VWSCR_CellGrid, VWStageRqMsg}
 
 import scala.collection.mutable
+import scala.util.Random
 
 /**
   * Created by Stub22 on 6/22/2016.
@@ -49,8 +51,20 @@ trait PatternGridMaker extends VarargsLogging {
 
 	}
 }
-trait VWSpatialsForShapes extends PatternGridMaker{
+trait SpatMatHelper {
 	protected def getTooMuchRRC : RenderRegistryClient
+	val rrc = getTooMuchRRC
+	val assetMgr = rrc.getJme3AssetManager(null);
+	val someMat = new Material(assetMgr, "Common/MatDefs/Misc/Unshaded.j3md") // someContent.makeAlphaBlendedUnshadedMaterial(rrc, 0f, 1.0f, 0, 0.5f);
+	val matPal = new MatPallete(someMat)
+	val outerGuy = new OuterGuy(rrc, matPal)
+	def getBrushJar : BrushJar = outerGuy.myBrushJar
+
+
+}
+trait VWSpatialsForShapes extends PatternGridMaker with SpatMatHelper {
+
+
 	//	val tsf: TextSpatialFactory = new TextSpatialFactory(rrc)
 	def makeForRq(vwscr : VWShapeCreateRq) : Spatial = {
 		vwscr match {
@@ -60,7 +74,7 @@ trait VWSpatialsForShapes extends PatternGridMaker{
 			case bigGrid : VWSCR_CellGrid => {
 				makeBigGridNode(getTooMuchRRC)
 			}
-			case meshBased : VWShapeCreateRq => {
+			case meshBased : VWMeshyShapeRq => {
 				val mesh: Mesh = meshBased match {
 					case sph: VWSCR_Sphere => {
 						// zSamp, rSamp, radius
@@ -69,18 +83,41 @@ trait VWSpatialsForShapes extends PatternGridMaker{
 				}
 				val geomNameArb: String = "geom_from_msg_shape_" + System.currentTimeMillis()
 				val geom = new Geometry(geomNameArb, mesh)
+				applyMat(geom, meshBased)
 				geom
 			}
 		}
 	}
+	def applyMat(geom : Geometry, srq : VWMeshyShapeRq) : Unit = {
+		val dsc : ColorRGBA = srq.dangerSerialColor
+		val brush = getBrushJar.makeBrush(dsc)
+		brush.stroke(geom)
+	}
 }
 
-case class MadeSpatRec(myID : Ident, mySpat : Spatial, myCreateRq : VWShapeCreateRq)
+case class MadeSpatRec(mySpat : Spatial, myID_opt : Option[Ident], myCreateRq : VWShapeCreateRq)
 
 trait VWShaperLogic extends PatternGridMaker {
+	val uniqueR = new Random()
+
 	protected def getRRC : RenderRegistryClient
-	protected val myTopDeepNode = getRRC.getJme3RootDeepNode(null)
-	protected val myTopFlatNode = getRRC.getJme3RootOverlayNode(null)
+	val noSuffix = ""
+	val idSuffix = "#id"
+	protected val myTopDeepNode : JmeNode = {
+		val nodeName = makeStampyRandyString("deep_shape_parent_", noSuffix)
+		val tdn = new JmeNode(nodeName)
+		val rootN = getRRC.getJme3RootDeepNode(null)
+		rootN.attachChild(tdn)
+		tdn
+	}
+	protected val myTopFlatNode : JmeNode = {
+		val nodeName = makeStampyRandyString("flat_shape_parent_", noSuffix)
+		val tfn = new JmeNode(nodeName)
+		val rootN = getRRC.getJme3RootOverlayNode(null)
+		rootN.attachChild(tfn)
+		tfn
+	}
+
 	protected val myAssetMgr = getRRC.getJme3AssetManager(null)
 
 	val myShapeMaker = new VWSpatialsForShapes{
@@ -92,27 +129,60 @@ trait VWShaperLogic extends PatternGridMaker {
 	protected def clearAllShapes_onRendThrd(): Unit = {
 		for (madeRec <- myMadeSpatRecsByID.values) {
 			val madeSpat = madeRec.mySpat
-			val parentNode = madeSpat.getParent()
-			if (parentNode != null) {
-				parentNode.detachChild(madeSpat)
-			}
+			val removed = madeSpat.removeFromParent()
+//			val parentNode = madeSpat.getParent()
+//			if (parentNode != null) {
+//				parentNode.detachChild(madeSpat)
+//			}
 		}
 		myMadeSpatRecsByID.clear()
+		myTopDeepNode.detachAllChildren()
+		myTopFlatNode.detachAllChildren()
 	}
-	def makeAndPlace(toMake : VWShapeCreateRq): Unit = {
-		val madeSpat = myShapeMaker.makeForRq(toMake)
-		val kuri_opt = toMake.getKnownURI_opt
-		if (kuri_opt.isDefined) {
-			val knownID = new FreeIdent(kuri_opt.get)
-			val madeSpatRec = new MadeSpatRec(knownID, madeSpat, toMake)
-			val previousMadeSpatRec_opt = myMadeSpatRecsByID.get(knownID)
-			if (toMake.expectUniqueURI && previousMadeSpatRec_opt.isDefined) {
-				warn2("Found existing shape-spat at oldShape={}, so IGNORING newShape={}",
-							previousMadeSpatRec_opt, toMake)
-			} else {
-				myMadeSpatRecsByID.put(knownID, madeSpatRec)
+	val makeIdentIfMissing : Boolean = true
+
+
+	private def makeStampyRandyString (prefix : String, suffix : String) : String = {
+		val rnum = uniqueR.nextInt(1000 * 1000)
+		val tstamp = System.currentTimeMillis()
+		String.format("%s%d_%06d%s", prefix, tstamp : java.lang.Long, rnum : Integer, suffix)
+	}
+
+	def makeAndPlace(toMake : VWShapeCreateRq): MadeSpatRec = {
+		val madeSpat : Spatial = myShapeMaker.makeForRq(toMake)
+		val madeSpatRec = registerSpat(madeSpat, toMake)
+		attachToParent(madeSpat, toMake)
+		madeSpatRec
+	}
+	protected def getOrMakeID_opt (toMake : VWShapeCreateRq) : Option[Ident] = {
+		val clientSentID_opt = toMake.getKnownID_opt
+		clientSentID_opt.orElse(if (makeIdentIfMissing) {
+			val aUriTxt = makeStampyRandyString("urn:ash_", idSuffix)
+			val id = new FreeIdent(aUriTxt)
+			Some(id)
+		} else None)
+	}
+	protected def registerSpat(madeSpat : Spatial, toMake : VWShapeCreateRq): MadeSpatRec = {
+		val assignedID_opt = getOrMakeID_opt(toMake)
+		val madeSpatRec = new MadeSpatRec(madeSpat, assignedID_opt, toMake)
+		// We only mess with the map, if there is no assignedID
+		if (assignedID_opt.isDefined) {
+			val assignedID: Ident = assignedID_opt.get
+			if (toMake.expectEmptySlot) {
+				val previousMadeSpatRec_opt = myMadeSpatRecsByID.get(assignedID)
+
+				if (previousMadeSpatRec_opt.isDefined) {
+					warn2("Found existing shape-spat at oldShape={}, so IGNORING newShape={}",
+						previousMadeSpatRec_opt, toMake)
+				}
 			}
+			info1("Storing madeSpatRec at id={}", assignedID)
+			myMadeSpatRecsByID.put(assignedID, madeSpatRec)
+
 		}
+		madeSpatRec
+	}
+	def attachToParent(madeSpat : Spatial, toMake : VWShapeCreateRq) : Unit = {
 		val knownParentID_opt = toMake.getKnownParentID_opt
 		val knownParentNode_opt : Option[JmeNode] = if (knownParentID_opt.isDefined) {
 			val kparid = knownParentID_opt.get
@@ -121,18 +191,20 @@ trait VWShaperLogic extends PatternGridMaker {
 				val pNode : JmeNode = knownParentMadeRec_opt.get.mySpat.asInstanceOf[JmeNode]
 				Option(pNode)
 			} else {
+				warn2("Could not find expected parentNode at ID={}, for child={}", kparid, toMake)
+				// TODO: Probably we should force failure in this case, not attach to default
 				None
 			}
 		} else None
 
-		val parentToUse_opt = knownParentNode_opt.orElse(if (toMake.inFlatSpace) {
+		val parentToUse_opt : Option[JmeNode] = knownParentNode_opt.orElse(if (toMake.inFlatSpace) {
 				Option(myTopFlatNode)
 			} else {
 			Option(myTopDeepNode)
-		})
-		if (parentToUse_opt.isDefined) {
-			parentToUse_opt.get.attachChild(madeSpat)
-		}
+			})
+		info2("Using parentNode={} for childSpatial={}", parentToUse_opt, madeSpat)
+		val dooda = parentToUse_opt.map(_.attachChild(madeSpat))
+
 	}
 	def attachBigGrid(): Unit = {
 		makeBigGridAndAttach_onRendThrd(getRRC, myTopDeepNode)
@@ -143,14 +215,14 @@ class VWShaperActor(myRRC: RenderRegistryClient) extends Actor with VWShaperLogi
 	override protected def getRRC : RenderRegistryClient = myRRC
 
 	def receive = {
-		case clearAll : VWCleaAllShapes => {
+		case clearAll : VWClearAllShapes => {
 			clearAllShapes_onRendThrd()
 		}
 		case cellGridRq : VWSCR_CellGrid => {
 			attachBigGrid
 		}
 		case vwsrq: VWShapeCreateRq => {
-			makeAndPlace(vwsrq)
+			val unusedResult : MadeSpatRec = makeAndPlace(vwsrq)
 		}
 	}
 }
