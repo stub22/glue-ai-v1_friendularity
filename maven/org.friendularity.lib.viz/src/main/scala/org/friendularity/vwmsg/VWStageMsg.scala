92da09e8a035053d1de097675637b90652c6a97c
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2016-07-04 23:16:20 +0000

    Progress towards controlling cameras via messages.

--- maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwmsg/VWStageMsg.scala
+++ maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/vwmsg/VWStageMsg.scala
@@ -1,28 +1,41 @@
 package org.friendularity.vwmsg
 
 import com.jme3.math.ColorRGBA
+import org.appdapter.core.name.Ident
+import org.friendularity.cpump.CPMsgTeller
 
 /**
-  * Created by Owner on 6/16/2016.
+  * Created by Stub22 on 6/16/2016.
   */
-// Used for messages about lights, cameras and related stagecraft
-trait VWStageRqMsg extends VWorldRequest {
-}
-
+// Used for messages about lights, cameras and related stagecraft - also messages for input keymap
+trait VWStageRqMsg extends VWorldRequest
 
 case class VWStageResetToDefault() extends VWStageRqMsg // Reset all camera + lighting effects to stored defaults
 case class VWStageEmulateBonusContentAndCams() extends VWStageRqMsg
 
 case class VWStageOpticsBasic(moveSpeed : Int, bgColor: ColorRGBA)  extends VWStageRqMsg
 
-// External callbacks should only send externaizable (VM-independent, String-encodable) messages at
-case class VWKeymapBinding_Extern(inpNamesToActionNames : Map[Char,String]) extends VWStageRqMsg
+// JME describes the coords as:   float left, float right, float bottom, float top) {
+case class ViewportDesc(myX1_left : Float, myX2_right : Float, myY1_bot : Float, myY2_top : Float,
+						myBGColor_opt : Option[ColorRGBA])
 
-// Medial = parallel an implemented in "OuterLogic".  Callbacks may send messages to internal, which may be nonserial in many cases
-case class VWKeymapBinding_Medial(inpNamesToActionFuncs : Map[String,Function1[VWorldPublicTellers,Unit]],
-								  pubTellers : VWorldPublicTellers) extends VWStageRqMsg
+// initXform is for the camera itself, and is in world coordinates.
+// Initially the camera is *not* the child of a parentNode, and thus does not have any local-coords notion.
+// Camera does not use scale xform, just pos and point.
 
-// We do not allow for a KeyBinding direct to Internal.  Stated another way.
-// We do not allow for direct callback from keyboard into code that talks to the RenderRegistryClient, JME3, etc.
-// That all must be done by actor messages triggered from the medial (or external) callback actions, responding to
-// the messages above.
+// Q: What happens to this xform if we Bind it, and then manipulate direction by *both* methods?
+case class VWCreateCamAndViewportRq(camID : Ident, initState: CamState3D, initVP : ViewportDesc) extends VWStageRqMsg
+
+// Again with the global xform state.
+case class VWModifyCamStateRq(camID : Ident, updState_opt : Option[CamState3D], updVP_opt: Option[ViewportDesc]) extends VWStageRqMsg
+
+// Must refer to an existing cam, which was either made by VWCreateCam_, or is known by some wellKnownID.
+// node will be attached as camera parent or child depending on flag.
+// Shape(node) is an example of a spaceNode.
+case class VWBindCamNodeRq(camID : Ident, spaceNodeIsCamParent_NotChild : Boolean, spaceTeller : CPMsgTeller, spaceNodeID : Ident) extends VWStageRqMsg
+
+// This can
+// case class VWCamNodeManipRq(camNodeID : Ident, manipGuts : ManipDesc) extends VWStageRqMsg
+
+
+
