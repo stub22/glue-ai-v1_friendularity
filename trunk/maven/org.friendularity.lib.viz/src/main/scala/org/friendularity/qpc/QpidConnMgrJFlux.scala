c31b416d0d8212a4912fed1e34ad47b2b8eef412
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2016-08-11 05:39:42 +0000

    Assigning unique stamp+random name to each Qpid client conn.  Disabled unnecessary remote client wiring and dependencies in CCMIO server demo.

--- trunk/maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/qpc/QpidConnMgrJFlux.scala
+++ trunk/maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/qpc/QpidConnMgrJFlux.scala
@@ -4,13 +4,14 @@ import javax.jms.{Session => JMSSession, Destination => JMSDestination, Connecti
 
 
 import org.appdapter.fancy.log.VarargsLogging
+import org.friendularity.vwimpl.IdentHlp
 import org.jflux.impl.messaging.rk.utils.{ConnectionManager => JfluxQpidConnMgr}
 
 
 /**
   * Created by Stub22  on 8/5/2016.
   */
-class QpidConnMgrJFlux extends QpidConnMgr with VarargsLogging {
+class QpidConnMgrJFlux extends QpidConnMgr with IdentHlp with VarargsLogging {
 
 	private lazy val myJmsConn : JMSConnection = makeOldeDfltLocalTestQpidConn
 
@@ -18,15 +19,15 @@ class QpidConnMgrJFlux extends QpidConnMgr with VarargsLogging {
 		var qpidConn: JMSConnection = null
 		val qpidUsr: String = "admin"
 		val qpidPsw: String = "admin"
-		val qpidCliName: String = "client1"
-		val qpidVHostName: String = "test"
+		val qpidCliName: String = makeStampyRandyString("qpidCli_", "")
+		val qpidVHostName: String = "test" // Qpid's default vhost name thru v0.26 was "test"
 		val qpidTcpUrl: String = "tcp://127.0.0.1:5672"
 		try {
 			qpidConn = JfluxQpidConnMgr.createConnection(qpidUsr, qpidPsw, qpidCliName, qpidVHostName, qpidTcpUrl)
 		}
 		catch {
 			case t: Throwable => {
-				error1("makeOldeDfltLocalTestQpidConn caught exception: {}", t)
+				error1("makeOldeDfltLocalTestQpidConn " + qpidCliName + " caught exception: {}", t)
 			}
 		}
 		return qpidConn
