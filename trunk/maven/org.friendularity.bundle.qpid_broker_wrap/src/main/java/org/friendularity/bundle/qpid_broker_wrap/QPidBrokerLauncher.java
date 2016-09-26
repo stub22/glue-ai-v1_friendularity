8ee97d3b8072a2fed0630d0eef7fd29cc3a573d6
stub22 <stub22@fd3addb2-b9ad-41f8-b59e-edbb4179333b>
2016-09-26 19:26:49 +0000

    QPid broker initial config now comes from file qpid_broker_home/our-initial-config.json,  instead of a classpath resource.

--- trunk/maven/org.friendularity.bundle.qpid_broker_wrap/src/main/java/org/friendularity/bundle/qpid_broker_wrap/QPidBrokerLauncher.java
+++ trunk/maven/org.friendularity.bundle.qpid_broker_wrap/src/main/java/org/friendularity/bundle/qpid_broker_wrap/QPidBrokerLauncher.java
@@ -16,6 +16,8 @@
 
 package org.friendularity.bundle.qpid_broker_wrap;
 
+import org.apache.qpid.server.BrokerOptions;
+import org.apache.qpid.server.model.VirtualHostNode;
 import org.appdapter.core.log.BasicDebugger;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
@@ -50,26 +52,63 @@ import org.slf4j.Logger;
  *
  * We have made this behavior optional.
  *
+ *
+ * ----
+ [java] 5466    INFO [FelixDispatchQueue] org.friendularity.bundle.qpid_broker_wrap.QPidBrokerLauncher (QPidBrokerLauncher.java:104) launchBrokerWithDirpaths - Arg[0]=-prop
+ [java] 5467    INFO [FelixDispatchQueue] org.friendularity.bundle.qpid_broker_wrap.QPidBrokerLauncher (QPidBrokerLauncher.java:104) launchBrokerWithDirpaths - Arg[1]=qpid.home_dir=qpid_broker_home/
+ [java] 5467    INFO [FelixDispatchQueue] org.friendularity.bundle.qpid_broker_wrap.QPidBrokerLauncher (QPidBrokerLauncher.java:104) launchBrokerWithDirpaths - Arg[2]=-prop
+ [java] 5467    INFO [FelixDispatchQueue] org.friendularity.bundle.qpid_broker_wrap.QPidBrokerLauncher (QPidBrokerLauncher.java:104) launchBrokerWithDirpaths - Arg[3]=qpid.work_dir=qpid_broker_work/
+ [java] 5467    INFO [FelixDispatchQueue] org.friendularity.bundle.qpid_broker_wrap.QPidBrokerLauncher (QPidBrokerLauncher.java:104) launchBrokerWithDirpaths - Arg[4]=--initial-config-path
+ [java] 5467    INFO [FelixDispatchQueue] org.friendularity.bundle.qpid_broker_wrap.QPidBrokerLauncher (QPidBrokerLauncher.java:104) launchBrokerWithDirpaths - Arg[5]=qpid_broker_home/our-initial-config.json
+ [java] 5468    INFO [FelixDispatchQueue] org.friendularity.bundle.qpid_broker_wrap.QPidBrokerLauncher (QPidBrokerLauncher.java:104) launchBrokerWithDirpaths - Arg[6]=-prop
+ [java] 5468    INFO [FelixDispatchQueue] org.friendularity.bundle.qpid_broker_wrap.QPidBrokerLauncher (QPidBrokerLauncher.java:104) launchBrokerWithDirpaths - Arg[7]=qpid.initial_config_virtualhost_config={ "type" : "Memory" }
  */
 public class QPidBrokerLauncher extends BasicDebugger {
 	private static Logger theLog = getLoggerForClass(QPidBrokerLauncher.class);
 
+	static private String OPT_PROP = "-prop";
+
+	// http://grepcode.com/file/repo1.maven.org/maven2/org.apache.qpid/qpid-broker/0.32/org/apache/qpid/server/Main.java
+	//      private static final Option OPTION_INITIAL_CONFIGURATION_PATH = OptionBuilder.withArgName("path").hasArg()
+	//             .withDescription("set the location of initial JSON config to use when creating/overwriting a broker configuration store")
+	// 				.withLongOpt("initial-config-path").create("icp");
+	static private String OPT_INIT_CONF_PATH = "--initial-config-path";
+
 	static public String DEFAULT_HOME_DIR_PATH = "qpid_broker_home/";
 	static public String DEFAULT_WORK_DIR_PATH = "qpid_broker_work/";
 
+	static public String DEFAULT_INITIAL_CONF_PATH = DEFAULT_HOME_DIR_PATH + "our-initial-config.json";
+
+	static public String DEFAULT_VHN_CONF = "{ \"type\" : \"Memory\" }";
+
+	// http://grepcode.com/file/repo1.maven.org/maven2/org.apache.qpid/qpid-broker-core/0.32/org/apache/qpid/server/BrokerOptions.java
+	// defines:
+	// public static final String QPID_WORK_DIR  = "qpid.work_dir";
+	// public static final String QPID_HOME_DIR  = "qpid.home_dir";
+
+	static private String QPID_HOME_PROP_NAME = BrokerOptions.QPID_HOME_DIR; // "qpid.home_dir"
+	static private String QPID_WORK_PROP_NAME = BrokerOptions.QPID_WORK_DIR; // "qpid.work_dir"
+
+	// https://apache.googlesource.com/qpid-java/+/refs/heads/trunk/broker-core/src/main/java/org/apache/qpid/server/model/VirtualHostNode.java
+	//  String QPID_INITIAL_CONFIG_VIRTUALHOST_CONFIG_VAR = "qpid.initial_config_virtualhost_config";
+
+	static private String QPID_VHN_PROP_NAME  = VirtualHostNode.QPID_INITIAL_CONFIG_VIRTUALHOST_CONFIG_VAR;
+
 	public static boolean launchBrokerWithDfltArgs(BundleContext bunCtx) {
-		return launchBrokerWithDirpaths(bunCtx, DEFAULT_HOME_DIR_PATH, DEFAULT_WORK_DIR_PATH);
+		return launchBrokerWithDirpaths(bunCtx, DEFAULT_HOME_DIR_PATH, DEFAULT_WORK_DIR_PATH,
+				DEFAULT_INITIAL_CONF_PATH);
 	}
 
 	public static boolean launchBrokerWithDirpaths(BundleContext bunCtx,
-				String homeDirPath, String workDirPath) {
+				String homeDirPath, String workDirPath, String initialConfPath) {
 		boolean successFlag = false;
-		// This arg setup can be done instead through System.Properties or env vars, whatever.
+		// This arg setup could  be done instead through System.Properties or env vars.
 		String[] pseudoArgs = {
-				"-prop", "qpid.home_dir=" + homeDirPath,
-				"-prop", "qpid.work_dir=" + workDirPath,
-				"-prop", "qpid.initial_config_virtualhost_config={ \"type\" : \"Memory\" }"
 
+				OPT_PROP,  				QPID_HOME_PROP_NAME + "=" + homeDirPath,
+				OPT_PROP,  				QPID_WORK_PROP_NAME + "=" + workDirPath,
+				OPT_PROP,  				QPID_VHN_PROP_NAME + "=" + DEFAULT_VHN_CONF,
+				OPT_INIT_CONF_PATH, 	initialConfPath
 		};
 
 		ClassLoader savedCL = Thread.currentThread().getContextClassLoader();
