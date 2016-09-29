820fd174610df24536d0e7553032d8de0748490c
matt.r.stevenson <1117794+matt-stevenson@users.noreply.github.com>
2016-09-29 20:01:33 +0000

    Friendularity headless bundles ServiceLaunchers now take a robotID in order to create separate sets of services for different robots.  All services created have IDs prefixed with the robotID.

--- maven/org.friendularity.bundle.headless.animation/src/main/java/org/friendularity/bundle/headless/animation/AnimServiceLauncher.java
+++ maven/org.friendularity.bundle.headless.animation/src/main/java/org/friendularity/bundle/headless/animation/AnimServiceLauncher.java
@@ -1,5 +1,7 @@
 package org.friendularity.bundle.headless.animation;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.appdapter.core.log.BasicDebugger;
 import org.jflux.impl.messaging.rk.lifecycle.JMSAvroAsyncReceiverLifecycle;
 import org.jflux.impl.messaging.rk.lifecycle.JMSAvroMessageSenderLifecycle;
@@ -41,21 +43,21 @@ public class AnimServiceLauncher extends BasicDebugger {
 	public final static String CONNECTION_ID = "animationConnection";
 	public final static String ANIM_DEST_ID = "animationRequest";
 	public final static String SIGNAL_DEST_ID = "animationSignal";
-	public final static String LIBRARY_ID = "myRobot";
+	public final static String LIBRARY_ID = "animationLibrary";
 	public final static String SIGNAL_SENDER_ID = "signalSender";
 
-	static public boolean theLaunchHasHappened = false;
+    private final static List<String> theLaunchedServiceIDs = new ArrayList<String>();
 
 	//  "Avatar_ZenoR50"
 	public static boolean launchServices(BundleContext bunCtx, Connection qpidConn, String animPlayerID)  {
-		if (theLaunchHasHappened) {
+		if (theLaunchedServiceIDs.contains(animPlayerID)) {
 			theLogger.error("Ignoring attempted duplicate launch of qpid conns for animation services, animPlayer={}", animPlayerID);
 			return false;
 		}
 		try {
 			AnimServiceLauncher asl = new AnimServiceLauncher();
 			asl.doLaunchServices(bunCtx, qpidConn, animPlayerID);
-			theLaunchHasHappened = true;
+			theLaunchedServiceIDs.add(animPlayerID);
 			return true;
 		}	catch (Throwable t) {
 			theLogger.error("Caught exception", t);
@@ -64,16 +66,19 @@ public class AnimServiceLauncher extends BasicDebugger {
 	}
 
 	public void doLaunchServices(BundleContext context, Connection qpidConn, String animPlayerID) throws Throwable {
-		connectAnimation(context, qpidConn);
+		connectAnimation(context, qpidConn, animPlayerID);
 		launchRemotePlayer(context, animPlayerID, ANIM_RECEIVER_ID,
 				SIGNAL_SENDER_ID, CONNECTION_ID, ANIM_DEST_ID);
-		AnimationPlayerHostLifecycle animPlayerHostLcyc = new AnimationPlayerHostLifecycle(animPlayerID, ANIM_RECEIVER_ID, SIGNAL_SENDER_ID);
+		AnimationPlayerHostLifecycle animPlayerHostLcyc = new AnimationPlayerHostLifecycle(
+                animPlayerID, 
+                animPlayerID + "_" + ANIM_RECEIVER_ID, 
+                animPlayerID + "_" + SIGNAL_SENDER_ID);
 		OSGiComponent<AnimationPlayerHostLifecycle> osgiComp = new OSGiComponent(context, animPlayerHostLcyc);
 		osgiComp.start();
-		launchAnimationLibrary(context, LIBRARY_ID);
+		launchAnimationLibrary(context, animPlayerID);
 	}
 
-	private void connectAnimation(BundleContext context, Connection qpidConn) throws Exception {
+	private void connectAnimation(BundleContext context, Connection qpidConn, String animPlayerID) throws Exception {
 		/*
         Connection qpidConn = ConnectionManager.createConnection(
                 "admin", "admin", "client1", "test",
@@ -82,48 +87,55 @@ public class AnimServiceLauncher extends BasicDebugger {
         */
 		theLogger.info("Registering Animation Connection and Destinations");
 		ConnectionUtils.ensureSession(context,
-				CONNECTION_ID, qpidConn, null);
+				animPlayerID + "_" + CONNECTION_ID, qpidConn, null);
 		ConnectionUtils.ensureDestinations(context,
-				ANIM_DEST_ID, "animationRequest", ConnectionUtils.TOPIC, null);
+				animPlayerID + "_" + ANIM_DEST_ID, "animationRequest", ConnectionUtils.TOPIC, null);
 		ConnectionUtils.ensureDestinations(context,
-				SIGNAL_DEST_ID, "animationSignal", ConnectionUtils.TOPIC, null);
+				animPlayerID + "_" + SIGNAL_DEST_ID, "animationSignal", ConnectionUtils.TOPIC, null);
 		theLogger.info("Animation Connection and Destinations Registered");
 	}
 
 	private void launchRemotePlayer(BundleContext context,
-									String playerId, String receiverId, String senderId, String conId, String destId){
+									String animPlayerID, String receiverId, String senderId, String conId, String destId){
 		JMSAvroMessageSenderLifecycle signalLife =
 				new JMSAvroMessageSenderLifecycle(
 						new PortableAnimationSignal.MessageRecordAdapter(),
 						AnimationSignal.class, AnimationSignallingRecord.class,
-						senderId, CONNECTION_ID, SIGNAL_DEST_ID);
+						animPlayerID + "_" + senderId, 
+                        animPlayerID + "_" + CONNECTION_ID, 
+                        animPlayerID + "_" + SIGNAL_DEST_ID);
 		ManagedService myAnimationSenderService =
 				new OSGiComponent(context, signalLife);
 		myAnimationSenderService.start();
 
 		theLogger.info("Launching Dynamic RemoteAnimationPlayerHost Service.");
 		new OSGiComponent(context,
-				new AnimationPlayerHostLifecycle(playerId, receiverId, senderId)
+				new AnimationPlayerHostLifecycle(animPlayerID, 
+                        animPlayerID + "_" + receiverId, 
+                        animPlayerID + "_" + senderId)
 		).start();
 
 		JMSAvroAsyncReceiverLifecycle reqRecLifecycle =
 				new JMSAvroAsyncReceiverLifecycle(
 						new PortableAnimationEvent.RecordMessageAdapter(),
 						AnimationEvent.class, AnimationEventRecord.class,
-						AnimationEventRecord.SCHEMA$, ANIM_RECEIVER_ID,
-						conId, destId);
+						AnimationEventRecord.SCHEMA$, 
+                        animPlayerID + "_" + ANIM_RECEIVER_ID,
+						animPlayerID + "_" + conId, 
+                        animPlayerID + "_" + destId);
 		OSGiComponent reqRec = new OSGiComponent(context, reqRecLifecycle);
 		reqRec.start();
 		theLogger.info("Dynamic RemoteAnimationPlayerHost Service Launched.");
 	}
 
-	private void launchAnimationLibrary(BundleContext context, String libraryId){
+	private void launchAnimationLibrary(BundleContext context, String animPlayerID){
 		theLogger.info("Launching AnimationLibrary Service.");
-		AnimationLibrary library = new DefaultAnimationLibrary(libraryId);
+		AnimationLibrary library = new DefaultAnimationLibrary(
+                animPlayerID + "_" + LIBRARY_ID);
 		ServiceRegistration reg = OSGiUtils.registerService(context,
 				AnimationLibrary.class.getName(),
 				AnimationLibrary.PROP_ANIM_PLAYER_ID,
-				libraryId, library, null);
+				animPlayerID + "_" + LIBRARY_ID, library, null);
 		theLogger.info("AnimationLibrary Service Launched.");
 	}
 
