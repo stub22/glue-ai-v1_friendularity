7a49772095f617ff147a0ac5e558b43f43438362
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2017-01-30 05:27:40 +0000

    TicTac mark position is now basically correct.

--- maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/netcli/goodtst/TestManyGoodyBursts.scala
+++ maven/org.friendularity.lib.viz/src/main/scala/org/friendularity/netcli/goodtst/TestManyGoodyBursts.scala
@@ -106,47 +106,46 @@ class TestManyGoodyBursts(ovwc : OffersVWorldClient) extends GoodyParamMaker wit
 	private val colorChoiceParams = List(grnTrans, prpTrans, ylwTrans)
 	private def ticTacStateSetParmsFunc(idx : Int) : SerTypedValueMap = {
 		val isOparm = makeXOStateParam(idx % 2 == 1) // idx-even => false = "X", idx-odd => true = "O"
-		val hPos = idx % 3
-		val vPos = (idx / 3) % 3
+		val hPos = (idx % 3) + 1   // ranges over 1,2,3
+		val vPos = ((idx / 3) % 3) + 1   // ranges over 1,2,3
 		val hParam = makeIntParam(GoodyNames.COORDINATE_X, hPos)
 		val vParam = makeIntParam(GoodyNames.COORDINATE_Y, vPos)
-		val colorParam = colorChoiceParams(hPos)
-		// val hLoc = make
+		val colorParam = colorChoiceParams(hPos - 1)
+
 		combineParams(List(isOparm, hParam, vParam, colorParam))
 	}
 	def fireTicTacGridBurst() : Unit = {
-		// val zPos = makeFloatParam(GoodyNames.LOCATION_Z, -10.0f)
 
-		val posParams = makeLoc3Params(-3.0f, 5.0f, -8.0f)
+		val posParams = makeLoc3Params(-20.0f, 15.0f, -8.0f)
 
 		// radIsNotUsed
 
 		val ttGridParams = combineParams(List(redTrans, rotUpParam,  posParams)) // turnAboutY_45deg
 
-		fireHorizBurst("horizTTGridOne", GT_TICTAC_GRID, ttGridParams, DFL_X_SPC * 3.0f, DFL_Y_DELT * 3.0f,
-					Some(ticTacStateSetParmsFunc), Some(wackyMoveRotParam), DFL_STP_DLY_OPT)
+		fireHorizBurst("horizTTGridOne", GT_TICTAC_GRID, ttGridParams, DFL_X_SPC * 4.0f, DFL_Y_DELT * 3.0f,
+					Some(ticTacStateSetParmsFunc), Some(wackyMoveRotParam), Some(700))
 	}
 	def fireTicXBurst() : Unit = {
+		// TicX is in the X-Z plane by default, so we rotate it up.
 		val posParams = makeLoc3Params(9.0f, 11.0f, -2.0f)
 		val isOparm = makeXOStateParam(false)
 		val ticXparms = combineParams(List(prpTrans, posParams, rotUpParam, isOparm))
 		fireHorizBurst_withWackyMoveRot("ticX", GT_TICTAC_MARK, ticXparms)
 	}
 	def fireTacOBurst() : Unit = {
+		// TicY is in the X-Y plane by default, so we don't rotate it.
 		val posParams = makeLoc3Params(7.0f, 3.0f, 2.0f)
 		val isOparm = makeXOStateParam(true)
-		val ticOparms = combineParams(List(ylwTrans, posParams, rotUpParam, isOparm))
+		val ticOparms = combineParams(List(ylwTrans, posParams, isOparm))
 		fireHorizBurst_withWackyMoveRot("tacO", GT_TICTAC_MARK, ticOparms)
 	}
 
 	def fireFloorBurst() : Unit = {
 		val posParams = makeLoc3Params(-5.0f, 1.5f, -2.0f)
-	//	val yPos = makeFloatParam(GoodyNames.LOCATION_Y, -3.0f)
 
 		val floorParams = combineParams(List(bluTrans, turnAboutZ_45deg, posParams))
 
 		fireHorizBurst("horizFloorOne", GT_FLOOR, floorParams, 6.0f, 3.0f, noSetParamsFunc, Some(wackyMoveRotParam),  Some(700))
-
 	}
 
 	def fireTextBurst() : Unit = {
@@ -180,15 +179,15 @@ class TestManyGoodyBursts(ovwc : OffersVWorldClient) extends GoodyParamMaker wit
 		fireHorizBurst("horizCross", GT_CROSSHAIR, chParams, 0.1f, 0.1f, noSetParamsFunc, noMoveRotParam, Some(400))
 	}
 	def fireSomeBursts(): Unit = {
-		fireTextBurst()
-
-		fireTicTacGridBurst()
-
-		fireFloorBurst()
-
 		fireTicXBurst()
 
 		fireTacOBurst()
+
+		fireTicTacGridBurst()
+
+		fireTextBurst()
+
+		fireFloorBurst()
 
 		fireBitCubeBurst()
 
